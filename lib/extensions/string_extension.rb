548493a91b2b23a0a0f39d5f3bbe7d0fdfb875bb
Jesus Laiz <zheileman@users.noreply.github.com>
2017-11-24 12:35:07 +0000

    Remove unused `to_bool` method

--- lib/extensions/string_extension.rb
+++ lib/extensions/string_extension.rb
@@ -6,10 +6,4 @@ module StringExtension
   def false?
     to_s.strip.empty? || /(false|f|no|n|0)$/i.match(to_s).present?
   end
-
-  def to_bool
-    return true  if true?
-    return false if false?
-    raise ArgumentError, "Invalid value for Boolean: `#{self}`"
-  end
 end
--- spec/lib/extensions/string_extension_spec.rb
+++ spec/lib/extensions/string_extension_spec.rb
@@ -1,27 +1,21 @@
 require 'rails_helper'
 
 describe String do
-  context 'when value is not a valid boolean' do
-    it 'raises an error' do
-      expect { 'x'.to_bool }.to raise_error(ArgumentError)
-    end
-  end
-
-  context 'to_bool' do
+  context '#true?' do
     context 'for blank values' do
       it 'should be false for empty string' do
-        expect(''.to_bool).to eq(false)
+        expect(''.true?).to eq(false)
       end
 
       it 'should be false for only spaces string' do
-        expect(' '.to_bool).to eq(false)
+        expect(' '.true?).to eq(false)
       end
     end
 
     context 'for truthy values' do
       %w(true t yes y 1).each do |value|
         it "should be true for '#{value}'" do
-          expect(value.to_bool).to eq(true)
+          expect(value.true?).to eq(true)
         end
       end
     end
@@ -29,7 +23,35 @@ describe String do
     context 'for falsey values' do
       %W(false f no n 0).each do |value|
         it "should be false for '#{value}'" do
-          expect(value.to_bool).to eq(false)
+          expect(value.true?).to eq(false)
+        end
+      end
+    end
+  end
+
+  context '#false?' do
+    context 'for blank values' do
+      it 'should be true for empty string' do
+        expect(''.false?).to eq(true)
+      end
+
+      it 'should be true for only spaces string' do
+        expect(' '.false?).to eq(true)
+      end
+    end
+
+    context 'for truthy values' do
+      %w(true t yes y 1).each do |value|
+        it "should be false for '#{value}'" do
+          expect(value.false?).to eq(false)
+        end
+      end
+    end
+
+    context 'for falsey values' do
+      %W(false f no n 0).each do |value|
+        it "should be true for '#{value}'" do
+          expect(value.false?).to eq(true)
         end
       end
     end
