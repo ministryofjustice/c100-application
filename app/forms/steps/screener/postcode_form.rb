12fa91ff656a7544a7891c4a84346434f468a3ec
Al Davidson <apdavidson@gmail.com>
2018-03-19 15:01:53 +0000

    add postcode format validation to screener form

--- app/forms/steps/screener/postcode_form.rb
+++ app/forms/steps/screener/postcode_form.rb
@@ -1,3 +1,18 @@
+class FullUkPostcodeValidator < ActiveModel::EachValidator
+  def parsed_postcode(postcode)
+    UKPostcode.parse(postcode)
+  end
+
+  def validate_each(record, attribute, value)
+    entries = value.split('\n').reject(&:blank?).compact
+    entries.each do |entry|
+      unless parsed_postcode(entry).full_valid?
+        record.errors[attribute] << I18n.t(:invalid, scope: [:errors, :attributes, :children_postcodes])
+      end
+    end
+  end
+end
+
 module Steps
   module Screener
     class PostcodeForm < BaseForm
@@ -6,6 +21,7 @@ module Steps
       has_one_association :screener_answers
 
       attribute :children_postcodes, String
+      validates :children_postcodes, full_uk_postcode: true
 
       private
 
