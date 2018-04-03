eff39c051c207c8cc04606b1bdd584647c00564e
Al Davidson <apdavidson@gmail.com>
2018-04-03 13:59:13 +0100

    reset without_notice details when without_notice is no

--- spec/forms/steps/application/without_notice_form_spec.rb
+++ spec/forms/steps/application/without_notice_form_spec.rb
@@ -2,4 +2,28 @@ require 'spec_helper'
 
 RSpec.describe Steps::Application::WithoutNoticeForm do
   it_behaves_like 'a yes-no question form', attribute_name: :without_notice
+
+  context 'when the user selects NO' do
+    let(:c100_application){ instance_double(C100Application, without_notice: without_notice) }
+    let(:without_notice){ GenericYesNo::NO }
+    let(:arguments){
+      {
+        c100_application: c100_application,
+        without_notice: GenericYesNo::NO
+      }
+    }
+    subject { described_class.new(arguments) }
+
+    it 'resets all details fields' do
+      expect(c100_application).to receive(:update).with({
+        without_notice: GenericYesNo::NO,
+        without_notice_details: nil,
+        without_notice_frustrate: nil,
+        without_notice_frustrate_details: nil,
+        without_notice_impossible: nil,
+        without_notice_impossible_details: nil,
+      })
+      subject.save
+    end
+  end
 end
