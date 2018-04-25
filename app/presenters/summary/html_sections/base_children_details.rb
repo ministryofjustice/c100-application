121b013151657843631d05255e5cc1d4d1578cd7
Al Davidson <apdavidson@gmail.com>
2018-04-25 16:05:30 +0100

    make change links work in child details summary

--- app/presenters/summary/html_sections/base_children_details.rb
+++ app/presenters/summary/html_sections/base_children_details.rb
@@ -3,26 +3,62 @@ module Summary
     class BaseChildrenDetails < Sections::BaseSectionPresenter
       protected
 
+      # rubocop:disable Metrics/MethodLength
       def personal_details(child)
         [
-          FreeTextAnswer.new(:child_full_name, child.full_name),
-          DateAnswer.new(:child_dob, child.dob),
-          FreeTextAnswer.new(:child_age_estimate, child.age_estimate), # This shows only if a value is present
-          Answer.new(:child_sex, child.gender),
+          FreeTextAnswer.new(
+            :child_full_name,
+            child.full_name,
+            change_path: edit_steps_children_names_path(child)
+          ),
+          DateAnswer.new(
+            :child_dob,
+            child.dob,
+            change_path: edit_child_details_path(child, 'dob_dd')
+          ),
+          FreeTextAnswer.new(
+            :child_age_estimate,
+            child.age_estimate,
+            change_path: edit_child_details_path(child, 'age_estimate')
+          ),
+          Answer.new(
+            :child_sex,
+            child.gender,
+            change_path: edit_child_details_path(child, 'gender_female')
+          ),
         ]
       end
+      # rubocop:enable Metrics/MethodLength
 
       def relationships(child)
         [
-          MultiAnswer.new(:child_applicants_relationship,  relation_to_child(child, c100.applicants)),
-          MultiAnswer.new(:child_respondents_relationship, relation_to_child(child, c100.respondents)),
+          MultiAnswer.new(:child_applicants_relationship,  relation_to_child(child, c100.applicants), change_path: edit_relation_path(child, c100.applicants)),
+          MultiAnswer.new(:child_respondents_relationship, relation_to_child(child, c100.respondents), change_path: edit_relation_path(child, c100.applicants)),
         ]
       end
 
       private
 
+      def edit_relation_path(child, people)
+        format("/steps/applicant/relationship/%<id>/child/%<child_id>",
+               id: relationship(child, people).pluck(:id),
+               child_id: child.id)
+      end
+
+      def edit_child_details_path(child, field_stub)
+        edit_steps_children_personal_details_path(
+          child,
+          anchor: format('steps_children_personal_details_form_%<field_stub>',
+                         field_stub)
+        )
+      end
+
       def relation_to_child(child, people)
-        child.relationships.where(person: people).pluck(:relation)
+        relationship(child, people).pluck(:relation)
+      end
+
+      def relationship(child, people)
+        child.relationships.where(person: people)
       end
     end
   end
--- app/presenters/summary/html_sections/children_details.rb
+++ app/presenters/summary/html_sections/children_details.rb
@@ -1,6 +1,6 @@
 module Summary
   module HtmlSections
-    class ChildrenDetails < Sections::BaseChildrenDetails
+    class ChildrenDetails < Summary::HtmlSections::BaseChildrenDetails
       def name
         :children_details
       end
@@ -8,9 +8,15 @@ module Summary
       def answers
         [
           children_details,
-          Answer.new(:children_known_to_authorities, c100.children_known_to_authorities),
-          FreeTextAnswer.new(:children_known_to_authorities_details, c100.children_known_to_authorities_details),
-          Answer.new(:children_protection_plan, c100.children_protection_plan),
+          Answer.new(:children_known_to_authorities,
+                     c100.children_known_to_authorities,
+                     change_path: edit_steps_children_additional_details_path),
+          FreeTextAnswer.new(:children_known_to_authorities_details,
+                             c100.children_known_to_authorities_details,
+                             change_path: edit_steps_children_additional_details_path),
+          Answer.new(:children_protection_plan,
+                     c100.children_protection_plan,
+                     change_path: edit_steps_children_additional_details_path),
         ].flatten.select(&:show?)
       end
 
@@ -22,7 +28,9 @@ module Summary
             Separator.new(:child_index_title, index: index),
             personal_details(child),
             relationships(child),
-            MultiAnswer.new(:child_orders, order_types(child)),
+            MultiAnswer.new(:child_orders,
+                            order_types(child),
+                            change_path: edit_steps_children_orders_path(child)),
           ]
         end
       end
