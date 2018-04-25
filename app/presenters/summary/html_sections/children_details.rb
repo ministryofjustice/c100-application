061b79400f2466e47febf11d41bd8577d2999210
Al Davidson <apdavidson@gmail.com>
2018-04-25 14:25:23 +0100

    first draft of children details in summary - child headings need work

---
+++ app/presenters/summary/html_sections/children_details.rb
@@ -0,0 +1,42 @@
+module Summary
+  module HtmlSections
+    class ChildrenDetails < Sections::BaseChildrenDetails
+      def name
+        :children_details
+      end
+
+      def answers
+        [
+          children_details,
+          Answer.new(:children_known_to_authorities, c100.children_known_to_authorities),
+          FreeTextAnswer.new(:children_known_to_authorities_details, c100.children_known_to_authorities_details),
+          Answer.new(:children_protection_plan, c100.children_protection_plan),
+        ].flatten.select(&:show?)
+      end
+
+      private
+
+      def children_details
+        children.map.with_index(1) do |child, index|
+          [
+            Separator.new(:child_index_title, index: index),
+            personal_details(child),
+            relationships(child),
+            MultiAnswer.new(:child_orders, order_types(child)),
+            #Partial.row_blank_space,
+          ]
+        end
+      end
+
+      def order_types(child)
+        child.child_order&.orders.to_a.map do |o|
+          PetitionOrder.type_for(o)
+        end.uniq
+      end
+
+      def children
+        @_children ||= c100.children
+      end
+    end
+  end
+end
