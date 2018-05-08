9e30e3fcc5566d74a2a96c391b97faf6966649e3
Al Davidson <apdavidson@gmail.com>
2018-05-08 13:59:03 +0100

    refactor to use AnswersGroup

--- spec/presenters/summary/html_sections/other_court_cases_spec.rb
+++ spec/presenters/summary/html_sections/other_court_cases_spec.rb
@@ -30,48 +30,22 @@ module Summary
     end
 
     describe '#answers' do
-      it 'has the correct rows' do
-        expect(answers.count).to eq(8)
+      context 'when there are previous proceedings' do
+        let(:children_previous_proceedings) { 'yes' }
 
-        expect(answers[0]).to be_an_instance_of(Answer)
-        expect(answers[0].question).to eq(:has_other_court_cases)
-        expect(answers[0].value).to eq('yes')
-        expect(answers[0].change_path).to eq('/steps/application/previous_proceedings')
+        it 'has the correct rows' do
+          expect(answers.count).to eq(2)
 
-        expect(answers[1]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[1].question).to eq(:court_proceeding_children_names)
-        expect(answers[1].value).to eq('children_names')
-        expect(answers[1].change_path).to eq('/steps/application/court_proceedings#steps_application_court_proceedings_form_children_names')
+          expect(answers[0]).to be_an_instance_of(Answer)
+          expect(answers[0].question).to eq(:has_other_court_cases)
+          expect(answers[0].value).to eq('yes')
+          expect(answers[0].change_path).to eq('/steps/application/previous_proceedings')
 
-        expect(answers[2]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[2].question).to eq(:court_proceeding_court_name)
-        expect(answers[2].value).to eq('court_name')
-        expect(answers[2].change_path).to eq('/steps/application/court_proceedings#steps_application_court_proceedings_form_court_name')
-
-        expect(answers[3]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[3].question).to eq(:court_proceeding_case_number)
-        expect(answers[3].value).to eq('case_number')
-        expect(answers[3].change_path).to eq('/steps/application/court_proceedings#steps_application_court_proceedings_form_case_number')
-
-        expect(answers[4]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[4].question).to eq(:court_proceeding_proceedings_date)
-        expect(answers[4].value).to eq('proceedings_date')
-        expect(answers[4].change_path).to eq('/steps/application/court_proceedings#steps_application_court_proceedings_form_proceedings_date')
-
-        expect(answers[5]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[5].question).to eq(:court_proceeding_cafcass_details)
-        expect(answers[5].value).to eq('cafcass_details')
-        expect(answers[5].change_path).to eq('/steps/application/court_proceedings#steps_application_court_proceedings_form_cafcass_details')
-
-        expect(answers[6]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[6].question).to eq(:court_proceeding_order_types)
-        expect(answers[6].value).to eq('order_types')
-        expect(answers[6].change_path).to eq('/steps/application/court_proceedings#steps_application_court_proceedings_form_order_types')
-
-        expect(answers[7]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[7].question).to eq(:court_proceeding_previous_details)
-        expect(answers[7].value).to eq('previous_details')
-        expect(answers[7].change_path).to eq('/steps/application/court_proceedings#steps_application_court_proceedings_form_previous_details')
+          expect(answers[1]).to be_an_instance_of(AnswersGroup)
+          expect(answers[1].name).to eq(:other_court_cases_details)
+          expect(answers[1].change_path).to eq('/steps/application/court_proceedings')
+          expect(answers[1].answers.size).to eq(7)
+        end
       end
 
       context 'when there are no previous proceedings' do
@@ -80,9 +54,45 @@ module Summary
         it 'has the correct rows' do
           expect(answers.count).to eq(1)
 
-          expect(answers[0]).to be_an_instance_of(Separator)
-          expect(answers[0].title).to eq(:not_applicable)
+          expect(answers[0]).to be_an_instance_of(Answer)
+          expect(answers[0].question).to eq(:has_other_court_cases)
+          expect(answers[0].value).to eq('no')
+          expect(answers[0].change_path).to eq('/steps/application/previous_proceedings')
         end
+      end
+    end
+
+    describe '#other_court_cases_details' do
+      let(:answers){ subject.send(:other_court_cases_details) }
+
+      it 'has the correct rows' do
+        expect(answers[0]).to be_an_instance_of(FreeTextAnswer)
+        expect(answers[0].question).to eq(:court_proceeding_children_names)
+        expect(answers[0].value).to eq('children_names')
+
+        expect(answers[1]).to be_an_instance_of(FreeTextAnswer)
+        expect(answers[1].question).to eq(:court_proceeding_court_name)
+        expect(answers[1].value).to eq('court_name')
+
+        expect(answers[2]).to be_an_instance_of(FreeTextAnswer)
+        expect(answers[2].question).to eq(:court_proceeding_case_number)
+        expect(answers[2].value).to eq('case_number')
+
+        expect(answers[3]).to be_an_instance_of(FreeTextAnswer)
+        expect(answers[3].question).to eq(:court_proceeding_proceedings_date)
+        expect(answers[3].value).to eq('proceedings_date')
+
+        expect(answers[4]).to be_an_instance_of(FreeTextAnswer)
+        expect(answers[4].question).to eq(:court_proceeding_cafcass_details)
+        expect(answers[4].value).to eq('cafcass_details')
+
+        expect(answers[5]).to be_an_instance_of(FreeTextAnswer)
+        expect(answers[5].question).to eq(:court_proceeding_order_types)
+        expect(answers[5].value).to eq('order_types')
+
+        expect(answers[6]).to be_an_instance_of(FreeTextAnswer)
+        expect(answers[6].question).to eq(:court_proceeding_previous_details)
+        expect(answers[6].value).to eq('previous_details')
       end
     end
   end
