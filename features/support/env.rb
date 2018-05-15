3d1f1f17fc4244f985f22d34cb46ee6c4ba5f08c
Al Davidson <apdavidson@gmail.com>
2018-05-15 16:21:14 +0100

    force Capybara.server_port too

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,8 +4,10 @@ require 'capybara/dsl'
 require 'capybara/cucumber'
 require 'capybara/poltergeist'
 require 'dotenv/load'
+require 'uri'
 
 Capybara.app_host = ENV.fetch('EXTERNAL_URL')
+Capybara.server_port = URI.parse(Capybara.app_host).port
 Capybara.run_server = false
 Capybara.default_driver = :poltergeist
 Capybara.javascript_driver = :poltergeist
