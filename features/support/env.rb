822b35eff4aa72347f75553d47d967ee931de752
Al Davidson <apdavidson@gmail.com>
2018-05-16 13:00:33 +0100

    reverting changes

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,24 +8,23 @@ require 'uri'
 
 #Capybara.app_host = ENV.fetch('EXTERNAL_URL')
 #Capybara.server_port = URI.parse(Capybara.app_host).port
-if false && ENV.fetch('TRAVIS', false) == 'true'
-  Capybara.run_server = true
-  Capybara.app_host = "http://127.0.0.1:8123"
-  Capybara.server_port = "8123"
-  Capybara.always_include_port = true
-else
+# if false && ENV.fetch('TRAVIS', false) == 'true'
+#   Capybara.run_server = true
+#   Capybara.app_host = "http://127.0.0.1:8123"
+#   Capybara.server_port = "8123"
+#   Capybara.always_include_port = true
+# else
   Capybara.run_server = false
   Capybara.app_host = ENV.fetch('EXTERNAL_URL')
-end
+# end
 Capybara.default_driver = :poltergeist
 Capybara.javascript_driver = :poltergeist
 
-puts "Capybara.app_host = #{Capybara.app_host}"
 options = {
   js_errors: false,
-  phantomjs_options: ['--disk-cache=true'],
-#  phantomjs: Phantomjs.path
+  phantomjs: Phantomjs.path
 }
+# phantomjs_options: ['--disk-cache=true'],
 
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, options)
