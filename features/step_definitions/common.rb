c65bf52f40d61c1a496e1cf56de1dfbacd2c6633
Al Davidson <apdavidson@gmail.com>
2018-05-16 10:28:30 +0100

    debug dump of page

--- features/step_definitions/common.rb
+++ features/step_definitions/common.rb
@@ -15,7 +15,7 @@ When(/^I visit "([^"]*)"$/) do |path|
   puts "---- /page response_parts dump ---"
 
   puts "page.html=#{page.html}"
-  puts "response = #{response.inspect}"
+  puts "page = #{page.inspect}"
 end
 
 Then(/^I should be on "([^"]*)"$/) do |page_name|
