98e56eca3baeda9f5e91bd1f3f5c1f8e2c714cf3
Petr Zaparka <petr@zaparka.cz>
2021-05-27 15:04:43 +0100

    Adding more test coverage

--- spec/forms/backoffice/lookup_form_spec.rb
+++ spec/forms/backoffice/lookup_form_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe Backoffice::LookupForm do
+  it { should validate_presence_of(:reference_code) }
+
   context 'reference_code transformation' do
     it "strip spaces" do
       form = described_class.new(reference_code: ' ABC1 ')
@@ -16,6 +18,15 @@ RSpec.describe Backoffice::LookupForm do
       expect do
         described_class.new(reference_code: nil)
       end.not_to raise_error
+    end
+
+    it "format valid" do
+      form = described_class.new(reference_code: '2021/05/CD7A69BC')
+      expect(form.valid?).to be true
+    end
+    it "format invalid" do
+      form = described_class.new(reference_code: 'CD7A69BC')
+      expect(form.valid?).to be false
     end
   end
 end
