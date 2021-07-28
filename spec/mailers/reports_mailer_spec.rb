aaa6eeb00ef75f98356146a1fef7e0212b4c2397
Jerome Riga <jriga@zemis.co.uk>
2021-07-28 12:03:24 +0100

    RST-3430 Add daily payment type report
    **JIRA**
    [RST-3430](https://tools.hmcts.net/jira/browse/RST-3430)
    
    **Description**
    Generate a daily report (csv file) with the number of applications for each of the payment types

--- spec/mailers/reports_mailer_spec.rb
+++ spec/mailers/reports_mailer_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe ReportsMailer, type: :mailer do
       Rails.configuration
     ).to receive(:govuk_notify_templates).and_return(
       failed_emails_report: 'failed_emails_report_template_id',
+      payment_types_report: 'payment_types_report_template_id',
     )
   end
 
@@ -20,6 +21,28 @@ RSpec.describe ReportsMailer, type: :mailer do
       expect(mail.govuk_notify_personalisation).to eq({
         service_name: 'Apply to court about child arrangements',
         report_content: 'report content',
+      })
+    end
+  end
+
+  describe '#payment_types_report' do
+    let(:mail) do
+      described_class.payment_types_report(
+        'Date,2021-12-20\npcd,10',
+        to_address: 'reports@example.com',
+        cc_address: 'copy@example.com'
+      )
+    end
+
+    it_behaves_like 'a Notify mail', template_id: 'payment_types_report_template_id'
+
+    it { expect(mail.to).to eq(['reports@example.com']) }
+    it { expect(mail.cc).to eq(['copy@example.com']) }
+
+    it 'has the right personalisation' do
+      expect(mail.govuk_notify_personalisation).to eq({
+        service_name: 'Apply to court about child arrangements',
+        link_to_report: { file: "RGF0ZSwyMDIxLTEyLTIwXG5wY2QsMTA=", is_csv: true},
       })
     end
   end
