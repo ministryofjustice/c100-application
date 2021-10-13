a1a699d28604b59f0a066eb75a7af101119d01d3
Petr Zaparka <petr@zaparka.cz>
2021-10-13 13:57:05 +0100

    Different URL for feedback on what next page

--- app/helpers/fees_helper.rb
+++ app/helpers/fees_helper.rb
@@ -4,4 +4,13 @@ module FeesHelper
       Rails.configuration.x.court_fee.amount_in_pence / 100
     )
   end
+
+  def phase_banner_copy
+    page = @c100_application.try(:navigation_stack).try(:last)
+    if page == "/steps/completion/what_next"
+      t('.feedback_completed_html')
+    else
+      t('.feedback_processing_html')
+    end
+  end
 end
