e88b23e76e015456a01113adfd32ba3bcda5588b
Petr Zaparka <petr@zaparka.cz>
2021-10-15 14:03:46 +0100

    Adding mediation voucher question

---
+++ app/presenters/summary/html_sections/mediation_voucher_requirement.rb
@@ -0,0 +1,16 @@
+module Summary
+  module HtmlSections
+    class MediationVoucherRequirement < Sections::BaseSectionPresenter
+      def name
+        :mediation_voucher
+      end
+
+      def answers
+        [
+          Answer.new(:mediation_voucher_scheme, c100.mediation_voucher_scheme,
+                     change_path: edit_steps_miam_acknowledgement_path)
+        ]
+      end
+    end
+  end
+end
