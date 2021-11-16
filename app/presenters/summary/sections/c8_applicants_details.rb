b44bf372ec1bab04658a23350b4682f507470231
Petr Zaparka <petr@zaparka.cz>
2021-11-16 14:41:58 +0000

    Listing private sections accordingly

--- app/presenters/summary/sections/c8_applicants_details.rb
+++ app/presenters/summary/sections/c8_applicants_details.rb
@@ -17,21 +17,48 @@ module Summary
       # applicants, most of the information is already disclosed in the C100, and here
       # we just need to show the confidential details. The superclass will show all.
       #
+      # modify this to list only the private details
       def answers
         record_collection.map.with_index(1) do |person, index|
           [
             Separator.new("#{name}_index_title", index: index),
             FreeTextAnswer.new(:person_full_name, person.full_name),
-            FreeTextAnswer.new(:person_address, person.full_address),
-            FreeTextAnswer.new(:person_email, person.email),
-            FreeTextAnswer.new(:person_home_phone, person.home_phone),
-            FreeTextAnswer.new(:person_mobile_phone, person.mobile_phone),
+            address(person),
+            person_email(person),
+            person_home_phone(person),
+            person_mobile_phone(person),
             Answer.new(:person_voicemail_consent, person.voicemail_consent),
             Partial.row_blank_space,
             FreeTextAnswer.new(:person_residence_history, person.residence_history),
             Partial.row_blank_space,
           ]
         end.flatten.select(&:show?)
+      end
+
+      private
+
+      def address(person)
+        value = (confidential? && person.residence_keep_private == 'yes') ? person.full_address : nil
+        FreeTextAnswer.new(:person_address, value)
+      end
+
+      def person_email(person)
+        value = (confidential? && person.email_keep_private == 'yes') ? person.email : nil
+        FreeTextAnswer.new(:person_email, value)
+      end
+
+      def person_home_phone(person)
+        value = (confidential? && person.phone_keep_private == 'yes') ? person.home_phone : nil
+        FreeTextAnswer.new(:person_home_phone, value)
+      end
+
+      def person_mobile_phone(person)
+        value = (confidential? && person.mobile_keep_private == 'yes') ? person.mobile_phone : nil
+        FreeTextAnswer.new(:person_mobile_phone, value)
+      end
+
+      def confidential?
+        c100.address_confidentiality == 'yes'
       end
     end
   end
