280fe7689e09ca00bccf8a0080cf066985003b21
Will Taylor <wrftaylor@gmail.com>
2022-01-18 09:20:25 +0000

    Separate people on relationships list on PDF generated

---
+++ app/presenters/summary/free_text_question_answer.rb
@@ -0,0 +1,7 @@
+module Summary
+  class FreeTextQuestionAnswer
+    def to_partial_path
+      'steps/completion/shared/row'
+    end
+  end
+end
--- app/presenters/summary/sections/children_relationships.rb
+++ app/presenters/summary/sections/children_relationships.rb
@@ -6,15 +6,20 @@ module Summary
       end
 
       def answers
-        [
+        c100.applicants.map.with_index(1) do |applicant, index|
           FreeTextAnswer.new(
             :applicants_relationships,
-            RelationshipsPresenter.new(c100_application).relationship_to_children(c100.applicants)
-          ),
+            RelationshipsPresenter.new(c100_application).relationship_to_children(applicant),
+            i18n_opts: { index: index }
+          )
+        end +
+        c100.respondents.map.with_index(1) do |respondent, index|
           FreeTextAnswer.new(
             :respondents_relationships,
-            RelationshipsPresenter.new(c100_application).relationship_to_children(c100.respondents)
-          ),
+            RelationshipsPresenter.new(c100_application).relationship_to_children(respondent),
+            i18n_opts: { index: index }
+          )
+        end + [
           FreeTextAnswer.new(
             :other_parties_relationships,
             RelationshipsPresenter.new(c100_application).relationship_to_children(c100.other_parties)
@@ -23,5 +28,8 @@ module Summary
         ].select(&:show?)
       end
     end
+
+
+    
   end
 end
