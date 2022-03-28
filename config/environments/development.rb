aa7f545a3468b08fc85c35c7e404ab80605dc676
Petr Zaparka <petr@zaparka.cz>
2022-03-28 14:42:16 +0100

    Fixing issue with missin url default

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,4 +25,7 @@ Rails.application.configure do
   # missing translations of model attribute names. The form will
   # get the constantized attribute name itself, in form labels.
   config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.default_url_options = { host: "localhost" }
+
 end
