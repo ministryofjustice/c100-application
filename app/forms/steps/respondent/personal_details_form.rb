549256b2f4848104e022eccbabf58443473da903
Will <wrftaylor@gmail.com>
2022-04-25 11:49:15 +0100

    Rst 4358 change dob in generated pdf add dob estimate validation (#1296)
    * RST-4358 Change age_estimate to date-based dob_estimate
    
    * Add DOB estimate validation

--- app/forms/steps/respondent/personal_details_form.rb
+++ app/forms/steps/respondent/personal_details_form.rb
@@ -17,6 +17,7 @@ module Steps
 
       validates_presence_of  :dob, unless: :dob_unknown?
       validates :dob, sensible_date: true, unless: :dob_unknown?
+      validates :dob_estimate, sensible_date: true, if: :dob_unknown?
 
       validates_presence_of :birthplace, unless: :birthplace_unknown?
 
