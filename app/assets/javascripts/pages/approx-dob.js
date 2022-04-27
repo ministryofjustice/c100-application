3e3fec1fa2097588054ea1dba54ba21a789a3cf4
Will <wrftaylor@gmail.com>
2022-04-27 11:47:57 +0100

    Rst 4358 add reasonable validations (#1300)

--- app/assets/javascripts/pages/approx-dob.js
+++ app/assets/javascripts/pages/approx-dob.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  document.querySelectorAll(".approx-dob input[type=text]").forEach(function(element){
+  document.querySelectorAll(".dob input[type=text]").forEach(function(element){
     setInputFilter(element, function(value) {
-      return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
-    }, "Only digits and '.' are allowed");        
+      return /^\d*$/.test(value); // Allow digits only, using a RegExp
+    }, "Only digits are allowed");
   })
 })
