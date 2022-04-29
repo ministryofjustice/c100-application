359356682cf131be8ddfca60b344e7b04442f1ae
Petr Zaparka <petr@zaparka.cz>
2022-04-29 10:40:16 +0100

    Using new FACT api endpoint

--- spec/presenters/valid_payments_array_spec.rb
+++ spec/presenters/valid_payments_array_spec.rb
@@ -59,7 +59,7 @@ RSpec.describe ValidPaymentsArray do
         'name' => 'Test court',
         'email' => 'court@example.com',
         'address' => 'Court address',
-        'cci_code' => 123,
+        'county_location_code' => 123,
         'slug' => court_slug,
         'gbs' => court_gbs,
       }
