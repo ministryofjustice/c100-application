8388154144f8dcf276fdbdf67b5bdf26b88b9e3f
Will <wrftaylor@gmail.com>
2022-04-29 12:42:28 +0100

    RST 3807 - DOB validations (#1309)

--- app/forms/steps/other_party/personal_details_form.rb
+++ app/forms/steps/other_party/personal_details_form.rb
@@ -15,7 +15,21 @@ module Steps
 
       validates_presence_of  :dob, unless: :dob_unknown?
       validates :dob, sensible_date: true, unless: :dob_unknown?
+      validates :input_dob, date: true, unless: :dob_unknown?
       validates :dob_estimate, sensible_date: true, if: :dob_unknown?
+      validates :input_dob_estimate, date: true, if: :dob_unknown?
+
+      # We have to save the date inputes to validate later
+      # because MultiParamDate will nil
+      # any invalid input date, and there is no clean way
+      # to validate an invalid date on entry
+      attr_accessor :input_dob, :input_dob_estimate
+
+      def initialize(args)
+        super(**args)
+        self.input_dob = args[:dob]
+        self.input_dob_estimate = args[:dob_estimate]
+      end
 
       private
 
