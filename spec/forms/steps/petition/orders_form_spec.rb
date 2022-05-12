ece82ac4ca61aa72b0d19286599f8bf50bb4ad5e
Will Taylor <wrftaylor@gmail.com>
2022-05-12 11:12:31 +0100

    RST-3309 remove additional details

--- spec/forms/steps/petition/orders_form_spec.rb
+++ spec/forms/steps/petition/orders_form_spec.rb
@@ -4,17 +4,15 @@ RSpec.describe Steps::Petition::OrdersForm do
   let(:arguments) { {
     c100_application: c100_application,
     orders: orders,
-    orders_collection: orders_collection,
-    orders_additional_details: orders_additional_details,
+    orders_collection: orders_collection
   } }
 
   let(:c100_application) {
-    instance_double(C100Application, orders: orders, orders_additional_details: orders_additional_details)
+    instance_double(C100Application, orders: orders)
   }
 
   let(:orders) { %w(child_arrangements_home group_prohibited_steps group_specific_issues) }
   let(:orders_collection) { %w(prohibited_steps_holiday specific_issues_medical) }
-  let(:orders_additional_details) { nil }
 
   subject { described_class.new(arguments) }
 
@@ -60,16 +58,6 @@ RSpec.describe Steps::Petition::OrdersForm do
   end
 
   describe 'validations' do
-    context 'when `other_issue` is checked' do
-      let(:orders) { ['other_issue'] }
-      it { should validate_presence_of(:orders_additional_details) }
-    end
-
-    context 'when `other_issue` is not checked' do
-      let(:orders) { [] }
-      it { should_not validate_presence_of(:orders_additional_details) }
-    end
-
     # mutant kill
     context 'when `orders` attribute is not submitted' do
       let(:arguments) { {
@@ -138,8 +126,7 @@ RSpec.describe Steps::Petition::OrdersForm do
     context 'when form is valid' do
       it 'saves the record' do
         expect(c100_application).to receive(:update).with(
-          orders: %w(prohibited_steps_holiday specific_issues_medical child_arrangements_home group_prohibited_steps group_specific_issues),
-          orders_additional_details: nil,
+          orders: %w(prohibited_steps_holiday specific_issues_medical child_arrangements_home group_prohibited_steps group_specific_issues)
         ).and_return(true)
 
         expect(subject.save).to be(true)
@@ -148,27 +135,10 @@ RSpec.describe Steps::Petition::OrdersForm do
       context 'when `another issue` checkbox is selected' do
         let(:orders) { %w(other_issue) }
         let(:orders_collection) { nil }
-        let(:orders_additional_details) { 'orders_additional_details' }
 
         it 'saves the record' do
           expect(c100_application).to receive(:update).with(
-            orders: %w(other_issue),
-            orders_additional_details: 'orders_additional_details',
-          ).and_return(true)
-
-          expect(subject.save).to be(true)
-        end
-      end
-
-      context 'when there are additional details but `another issue` checkbox is not selected' do
-        let(:orders) { %w(group_prohibited_steps) }
-        let(:orders_collection) { %w(prohibited_steps_holiday) }
-        let(:orders_additional_details) { 'orders_additional_details' }
-
-        it 'resets the content `orders_additional_details` attribute' do
-          expect(c100_application).to receive(:update).with(
-            orders: %w(prohibited_steps_holiday group_prohibited_steps),
-            orders_additional_details: nil,
+            orders: %w(other_issue)
           ).and_return(true)
 
           expect(subject.save).to be(true)
@@ -178,14 +148,12 @@ RSpec.describe Steps::Petition::OrdersForm do
       context 'for a consent order' do
         let(:arguments) { {
           c100_application: c100_application,
-          orders: %w(consent_order),
-          orders_additional_details: '',
+          orders: %w(consent_order)
         } }
 
         it 'saves the record' do
           expect(c100_application).to receive(:update).with(
-            orders: %w(consent_order),
-            orders_additional_details: nil,
+            orders: %w(consent_order)
           ).and_return(true)
 
           expect(subject.save).to be(true)
