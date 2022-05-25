62ee32e1ec94d62a6e1897b7d36ae52223d75712
Will Taylor <wrftaylor@gmail.com>
2022-05-25 14:53:53 +0100

    Rubocop

--- app/mailers/reports_mailer.rb
+++ app/mailers/reports_mailer.rb
@@ -26,9 +26,9 @@ class ReportsMailer < NotifyMailer
 
   private
 
-    def log(message)
-      @slack ||= Slack::Incoming::Webhooks.new ENV.fetch('SLACK_WEBHOOK_URL')
-      @slack.post "#{ENV.fetch('SLACK_WEBHOOK_ENV')}: #{message}"
-      Rails.logger.info message
-    end
+  def log(message)
+    @slack ||= Slack::Incoming::Webhooks.new ENV.fetch('SLACK_WEBHOOK_URL')
+    @slack.post "#{ENV.fetch('SLACK_WEBHOOK_ENV')}: #{message}"
+    Rails.logger.info message
+  end
 end
