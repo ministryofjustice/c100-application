119e6ea660981f6426b705e5ed64e4fad3e152bd
Will Taylor <wrftaylor@gmail.com>
2022-05-25 14:49:04 +0100

    RST-4416 Add slack integration to track down bug

--- app/mailers/reports_mailer.rb
+++ app/mailers/reports_mailer.rb
@@ -10,14 +10,25 @@ class ReportsMailer < NotifyMailer
   end
 
   def payment_types_report(report_csv, to_address:, cc_address:)
+    log 'starting payment_types_report'
     set_template(:payment_types_report)
+    log 'setting personalisation'
     set_personalisation(
       link_to_report: Notifications.prepare_upload(
         StringIO.new(report_csv),
         true
       )
     )
+    log 'send mail'
 
     mail(to: to_address, cc: cc_address)
   end
+
+  private
+
+    def log(message)
+      @slack ||= Slack::Incoming::Webhooks.new ENV.fetch('SLACK_WEBHOOK_URL')
+      @slack.post "#{ENV.fetch('SLACK_WEBHOOK_ENV')}: #{message}"
+      Rails.logger.info message
+    end
 end
