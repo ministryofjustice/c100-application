d90f220665f288a26b930fd753a9028421a949aa
Will <wrftaylor@gmail.com>
2022-05-31 12:01:29 +0100

    RST-4345 Fix duplicate child info when hitting back button (#1333)
    * RST-4345 Fix duplicate child info when hitting back button
    
    * Fix rubocop issues
    
    * Update names_crud_step.rb

--- lib/extensions/relation_extension.rb
+++ lib/extensions/relation_extension.rb
@@ -2,11 +2,11 @@
 # Refer to https://github.com/rails/rails/pull/31989
 
 module RelationExtension
-  #:nocov:
+  # :nocov:
   def create_or_find_by!(attributes, &block)
     transaction(requires_new: true) { create!(attributes, &block) }
   rescue ActiveRecord::RecordNotUnique
     find_by!(attributes)
   end
-  #:nocov:
+  # :nocov:
 end
