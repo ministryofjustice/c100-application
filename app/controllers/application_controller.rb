3255362cbbdc7d9901911cadaf157abee67c351b
Will Taylor <wrftaylor@gmail.com>
2022-08-23 14:01:35 +0100

    log everything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   include ErrorHandling
 
   before_action :show_maintenance_page
+  around_action :log_everything
 
   # This is required to get request attributes in to the production logs.
   # See the various lograge configurations in `production.rb`.
@@ -46,4 +47,28 @@ class ApplicationController < ActionController::Base
     render 'static_pages/maintenance', status: :service_unavailable
   end
   # :nocov:
+
+  # :nocov:
+  def log_everything
+    log_headers
+    yield
+  ensure
+    # log_response
+  end
+
+  def log_headers
+    http_envs = {}.tap do |envs|
+      request.headers.each do |key, value|
+        envs[key] = value if key.downcase.starts_with?('http')
+      end
+    end
+
+    logger.info "Received #{request.method.inspect} to #{request.url.inspect} from #{request.remote_ip.inspect}. Processing with headers #{http_envs.inspect} and params #{params.inspect}"
+  end
+
+  # def log_response
+  #   logger.info "Responding with #{response.status.inspect} => #{response.body.inspect}"
+  # end
+  # :nocov:
+
 end
