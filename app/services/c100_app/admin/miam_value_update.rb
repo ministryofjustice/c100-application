7b7ecbef22fcf2108d770eea49b7e3e820c36bd3
kalvinlyu <kalvin.lyu@solirius.com>
2022-10-06 16:39:48 +0100

    RST-4646
    Refactored migration into a service
    
    tests for service to ensure migration passes
    
    Chnaged migration to reference service

---
+++ app/services/c100_app/admin/miam_value_update.rb
@@ -0,0 +1,57 @@
+module C100App
+  module Admin
+    class MiamValueUpdate
+      def update_miam_values!
+        attribute_renames = {:adr => %w[previous_attendance ongoing_attendance existing_proceedings_attendance previous_exemption existing_proceedings_exemption adr_none],
+                             :domestic => %w[right_to_remain financial_abuse domestic_none],
+                             :misc => %w[no_respondent_address without_notice access_prohibited non_resident applicant_under_age],
+                             :protection => %w[authority_enquiring authority_protection_order protection_none],
+                             :urgency => %w[risk_applicant unreasonable_hardship risk_children risk_unlawful_removal_retention miscarriage_justice irretrievable_problems international_proceedings urgency_none] }
+        attribute_renames.each do |attribute_key, attribute_array|
+          id = MiamExemption.pluck(:id)
+          column = MiamExemption.pluck(attribute_key)
+          count = 0
+          column.each_with_object(new_column = []) do |set|
+            set.each do |entry|
+              new_column << if attribute_array.include?(entry)
+                              "misc_#{entry}"
+                            else
+                              entry
+                            end
+            end
+            new_column.flatten!
+            MiamExemption.where("id = '#{id[count]}'").update(attribute_key => new_column)
+            new_column = []
+            count += 1
+          end
+        end
+      end
+
+      def undo_miam_migrate!
+        misc_attribute_renames = {:adr => %w[misc_previous_attendance misc_ongoing_attendance misc_existing_proceedings_attendance misc_previous_exemption misc_existing_proceedings_exemption misc_adr_none],
+                                  :domestic => %w[misc_right_to_remain misc_financial_abuse misc_domestic_none],
+                                  :misc => %w[misc_no_respondent_address misc_without_notice misc_access_prohibited misc_non_resident misc_applicant_under_age],
+                                  :protection => %w[misc_authority_enquiring misc_authority_protection_order misc_protection_none],
+                                  :urgency => %w[misc_risk_applicant misc_unreasonable_hardship misc_risk_children misc_risk_unlawful_removal_retention misc_miscarriage_justice misc_irretrievable_problems misc_international_proceedings misc_urgency_none] }
+        misc_attribute_renames.each do |attribute_key, attribute_array|
+          id = MiamExemption.pluck(:id)
+          column = MiamExemption.pluck(attribute_key)
+          count = 0
+          column.each_with_object(new_column = []) do |set|
+            set.each do |entry|
+              new_column << if attribute_array.include?(entry)
+                              entry.delete_prefix("misc_")
+                            else
+                              entry
+                            end
+            end
+            new_column.flatten!
+            MiamExemption.where("id = '#{id[count]}'").update(attribute_key => new_column)
+            new_column = []
+            count += 1
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/services/c100_app/admin/miam_value_update_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe C100App::Admin::MiamValueUpdate do
+  miam = MiamExemption.create(:domestic => %w[right_to_remain financial_abuse], :adr => %w[previous_exemption],
+                                    :urgency => %w[risk_children], :misc => %w[misc_none without_notice])
+  c100_application = C100Application.create(miam_exemption: miam)
+
+  miam2 = MiamExemption.create(:domestic => %w[right_to_remain financial_abuse], :adr => %w[adr_none],
+                               :misc => %w[misc_none access_prohibited non_resident])
+  c100_application2 = C100Application.create(miam_exemption: miam2)
+
+  after(:all) do
+    c100_application.destroy
+    c100_application2.destroy
+    miam.destroy
+    miam2.destroy
+  end
+
+  it 'converts miam exemptions' do
+    subject.update_miam_values!
+
+    miam.reload
+    miam2.reload
+
+    expect(miam.domestic).to eq(%w[misc_right_to_remain misc_financial_abuse])
+    expect(miam.adr).to eq(%w[misc_previous_exemption])
+    expect(miam.urgency).to eq(%w[misc_risk_children])
+    expect(miam.misc).to eq(%w[misc_none misc_without_notice])
+
+    expect(miam2.domestic).to eq(%w[misc_right_to_remain misc_financial_abuse])
+    expect(miam2.adr).to eq(%w[misc_adr_none])
+    expect(miam2.urgency).to eq([])
+    expect(miam2.misc).to eq(%w[misc_none misc_access_prohibited misc_non_resident])
+  end
+
+  it 'reverts the miam values' do
+    subject.update_miam_values!
+    subject.undo_miam_migrate!
+
+    miam.reload
+    miam2.reload
+
+    expect(miam.domestic).to eq(%w[right_to_remain financial_abuse])
+    expect(miam.adr).to eq(%w[previous_exemption])
+    expect(miam.urgency).to eq(%w[risk_children])
+    expect(miam.misc).to eq(%w[misc_none without_notice])
+
+    expect(miam2.domestic).to eq(%w[right_to_remain financial_abuse])
+    expect(miam2.adr).to eq(%w[adr_none])
+    expect(miam2.misc).to eq(%w[misc_none access_prohibited non_resident])
+  end
+end
