76e86313eb788b864da54d4610a970c70b2b1bca
kalvinlyu <kalvin.lyu@solirius.com>
2022-10-07 10:26:01 +0100

    RST-4646
    Rubocop fixes

--- app/services/c100_app/admin/miam_value_update.rb
+++ app/services/c100_app/admin/miam_value_update.rb
@@ -2,29 +2,29 @@ module C100App
   module Admin
     class MiamValueUpdate
       def update_values
-        attribute_renames = {adr: %w[previous_attendance ongoing_attendance existing_proceedings_attendance
-                                     previous_exemption existing_proceedings_exemption adr_none],
-                             domestic: %w[right_to_remain financial_abuse domestic_none],
-                             misc: %w[no_respondent_address without_notice access_prohibited
-                                      non_resident applicant_under_age],
-                             protection: %w[authority_enquiring authority_protection_order protection_none],
-                             urgency: %w[risk_applicant unreasonable_hardship risk_children
-                                         risk_unlawful_removal_retention miscarriage_justice irretrievable_problems
-                                         international_proceedings urgency_none] }
+        {adr: %w[previous_attendance ongoing_attendance existing_proceedings_attendance
+                 previous_exemption existing_proceedings_exemption adr_none], 
+         domestic: %w[right_to_remain financial_abuse domestic_none],
+         misc: %w[no_respondent_address without_notice access_prohibited
+                  non_resident applicant_under_age],
+         protection: %w[authority_enquiring authority_protection_order protection_none],
+         urgency: %w[risk_applicant unreasonable_hardship risk_children
+                     risk_unlawful_removal_retention miscarriage_justice irretrievable_problems
+                     international_proceedings urgency_none] }
       end
 
       def undo_values
-        misc_attribute_renames = {adr: %w[misc_previous_attendance misc_ongoing_attendance
-                                          misc_existing_proceedings_attendance misc_previous_exemption
-                                          misc_existing_proceedings_exemption misc_adr_none],
-                                  domestic: %w[misc_right_to_remain misc_financial_abuse misc_domestic_none],
-                                  misc: %w[misc_no_respondent_address misc_without_notice misc_access_prohibited
-                                           misc_non_resident misc_applicant_under_age],
-                                  protection: %w[misc_authority_enquiring misc_authority_protection_order
-                                                 misc_protection_none],
-                                  urgency: %w[misc_risk_applicant misc_unreasonable_hardship misc_risk_children
-                                              misc_risk_unlawful_removal_retention misc_miscarriage_justice
-                                              misc_irretrievable_problems misc_international_proceedings misc_urgency_none] }
+        {adr: %w[misc_previous_attendance misc_ongoing_attendance
+                 misc_existing_proceedings_attendance misc_previous_exemption
+                 misc_existing_proceedings_exemption misc_adr_none],
+         domestic: %w[misc_right_to_remain misc_financial_abuse misc_domestic_none],
+         misc: %w[misc_no_respondent_address misc_without_notice misc_access_prohibited
+                  misc_non_resident misc_applicant_under_age],
+         protection: %w[misc_authority_enquiring misc_authority_protection_order
+                        misc_protection_none],
+         urgency: %w[misc_risk_applicant misc_unreasonable_hardship misc_risk_children
+                     misc_risk_unlawful_removal_retention misc_miscarriage_justice
+                     misc_irretrievable_problems misc_international_proceedings misc_urgency_none] }
       end
 
       def update_miam_values!
