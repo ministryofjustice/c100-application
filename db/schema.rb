d22f4624306417a1acb0073fa96a344c963afbc0
kalvinlyu <kalvin.lyu@solirius.com>
2022-10-12 10:05:41 +0100

    RST-4646
    Add schema to PR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_20_144235) do
+ActiveRecord::Schema.define(version: 2022_10_05_130946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
