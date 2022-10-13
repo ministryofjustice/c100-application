8f9fc4ae93c2da8c9aad9c252af42d1fa2a5e142
kalvinlyu <kalvin.lyu@solirius.com>
2022-10-13 16:30:26 +0100

    RST-4646
    Migration error fix: added .new

--- db/migrate/20221005130946_update_miam_exemption_misc_values.rb
+++ db/migrate/20221005130946_update_miam_exemption_misc_values.rb
@@ -1,9 +1,9 @@
 class UpdateMiamExemptionMiscValues < ActiveRecord::Migration[6.0]
   def up
-    Miam::MiamValueUpdate.update_miam_values!
+    Miam::MiamValueUpdate.new.update_miam_values!
   end
 
   def down
-    Miam::MiamValueUpdate.undo_miam_migrate!
+    Miam::MiamValueUpdate.new.undo_miam_migrate!
   end
 end
