2ee542b098c32d4e8c752f01437c82575c252006
kalvinSolirius <104897244+kalvinSolirius@users.noreply.github.com>
2022-10-17 16:05:39 +0100

    RST-4694 (#1375)
    Added under 18 validation for child age
    
    Added new error message for under 18 validation

--- app/forms/steps/children/personal_details_form.rb
+++ app/forms/steps/children/personal_details_form.rb
@@ -15,7 +15,8 @@ module Steps
       validates :dob_estimate, sensible_date: true, if: :dob_unknown?
       validates :input_dob_estimate, date: true, if: :dob_unknown?
       validates :input_dob, blank_date_input: true, if: :dob_unknown?
-
+      validate :validate_dob_under_18, unless: :dob_unknown?
+      validate :validate_dob_estimate_under_18, if: :dob_unknown?
       # We have to save the date inputes to validate later
       # because MultiParamDate will nil
       # any invalid input date, and there is no clean way
@@ -30,6 +31,18 @@ module Steps
       end
 
       private
+
+      def validate_dob_under_18
+        return if dob.nil?
+
+        errors.add(:dob, :under_18) unless dob > 18.years.ago
+      end
+
+      def validate_dob_estimate_under_18
+        return if dob_estimate.nil?
+
+        errors.add(:dob_estimate, :under_18) unless dob_estimate > 18.years.ago
+      end
 
       def persist!
         raise C100ApplicationNotFound unless c100_application
