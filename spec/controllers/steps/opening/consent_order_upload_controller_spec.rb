8539a0ba2beb3e5e04bfd1080eb447c17e5d9deb
Will Taylor <wrftaylor@gmail.com>
2022-10-27 11:39:47 +0100

    add tests for controller

--- spec/controllers/steps/opening/consent_order_upload_controller_spec.rb
+++ spec/controllers/steps/opening/consent_order_upload_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Steps::Opening::ConsentOrderUploadController, type: :controller, focus: true do
-  it_behaves_like 'an intermediate step controller', Steps::Opening::ConsentOrderUploadForm, C100App::OpeningDecisionTree
+RSpec.describe Steps::Opening::ConsentOrderUploadController, type: :controller do
+  it_behaves_like 'an intermediate step controller', Steps::Opening::ConsentOrderUploadForm, C100App::OpeningDecisionTree do
+    before do
+      allow(controller).to receive(:court_sanity_check).and_return(true)
+    end
+  end
 end
