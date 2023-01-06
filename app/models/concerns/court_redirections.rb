17cdaac84af29cc05b3cc7856a0cf4dde466f4d4
kalvinlyu <kalvin.lyu@solirius.com>
2023-01-06 10:47:54 +0000

    RST-4567
    Re-added accidentally deleted code

--- app/models/concerns/court_redirections.rb
+++ app/models/concerns/court_redirections.rb
@@ -4,6 +4,7 @@ module CourtRedirections
   def self.included(base)
     base.class_eval do
       after_save :redirect_urgent_hearings
+      after_save :revert_non_urgent_hearings
     end
   end
 
@@ -11,14 +12,43 @@ module CourtRedirections
   # Urgent hearings by West London Family Court
   # redirected to Barnet Civil and Family Courts
   def redirect_urgent_hearings
-    return if urgent_hearing != 'yes' ||
-              !court ||
-              court.id != 'west-london-family-court'
+    if urgent_hearing == 'yes' &&
+      court &&
+      court.id == 'west-london-family-court'
+      update(court: barnet_civil_and_family_courts_centre)
+    end
+  end
 
-    update(court: barnet_civil_and_family_courts_centre)
+  def revert_non_urgent_hearings
+    if urgent_hearing == 'no' &&
+      court &&
+      court.id == 'barnet-civil-and-family-courts-centre'
+      update(court: west_london_family_court)
+    end
   end
 
   private
+
+  def west_london_family_court
+    Court.find_or_create_by(id: 'west-london-family-court') do |court|
+      court.name = "West London Family Court"
+      court.gbs = "Y691"
+      court.cci_code = nil
+      court.address = {
+        "town" => "Feltham",
+        "type" => "Visit or contact us",
+        "county" => "Greater London",
+        "postcode" => "TW14 0LR",
+        "description" => nil,
+        "address_lines" => [
+          "Gloucester House",
+          "4 Dukes Green Avenue"
+        ],
+        "fields_of_law" => nil
+      }
+      court.email = "westlondonfamilyenquiries@justice.gov.uk"
+    end
+  end
 
   def barnet_civil_and_family_courts_centre
     Court.find_or_create_by(id: 'barnet-civil-and-family-courts-centre') do |court|
