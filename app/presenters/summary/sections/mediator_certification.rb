250280b2ba8883b206e0ede28995148499f59a09
kalvinlyu <kalvin.lyu@solirius.com>
2023-01-31 15:07:35 +0000

    RST-5052
    Corrected pdf content for secion 4
    
    Added tests to spec to ensure correct values

--- app/presenters/summary/sections/mediator_certification.rb
+++ app/presenters/summary/sections/mediator_certification.rb
@@ -15,8 +15,16 @@ module Summary
         ] unless c100.miam_certification.eql?(GenericYesNo::YES.to_s)
 
         [
+          FreeTextAnswer.new(:miam_certificate, maim_certificate_answer),
           Separator.new(:hmcts_instructions),
         ].select(&:show?)
+      end
+
+      private
+
+      def maim_certificate_answer
+        c100.documents(:miam_certificate).any? &&
+          I18n.t('check_answers.miam_certificate.answer')
       end
     end
   end
