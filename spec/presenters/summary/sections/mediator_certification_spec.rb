16f1e49ff0c4bcd32290fc798ebc5041b53b2851
kalvinSolirius <104897244+kalvinSolirius@users.noreply.github.com>
2023-02-01 09:26:53 +0000

    RST-5052 (#1449)
    Corrected pdf content for secion 4
    
    Added tests to spec to ensure correct values

--- spec/presenters/summary/sections/mediator_certification_spec.rb
+++ spec/presenters/summary/sections/mediator_certification_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 module Summary
   describe Sections::MediatorCertification do
-    let(:c100_application) { instance_double(C100Application, miam_certification: miam_certification) }
+    let(:c100_application) { instance_double(C100Application, miam_certification: miam_certification,
+                                             documents: documents) }
 
     let(:miam_certification) { 'yes' }
 
     subject { described_class.new(c100_application) }
 
     let(:answers) { subject.answers }
+    let(:documents) { [double(name: 'file.jpg')] }
 
     describe '#name' do
       it { expect(subject.name).to eq(:mediator_certification) }
@@ -20,10 +22,12 @@ module Summary
 
     describe '#answers' do
       it 'has the correct rows' do
-        expect(answers.count).to eq(1)
+        expect(answers.count).to eq(2)
 
-        expect(answers[0]).to be_an_instance_of(Separator)
-        expect(answers[0].title).to eq(:hmcts_instructions)
+        expect(answers[0].question).to eq(:miam_certificate)
+        expect(answers[0].value).to eq('[ File(s) uploaded with application ]')
+        expect(answers[1]).to be_an_instance_of(Separator)
+        expect(answers[1].title).to eq(:hmcts_instructions)
       end
 
       context 'when no certification received' do
