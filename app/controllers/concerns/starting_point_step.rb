1c292950a0aeaa6753e06ec75e34b4aa7a7d41d4
Will <wrftaylor@gmail.com>
2023-04-24 17:05:41 +0100

    temporarily remove rst-5258 (#1475)
    * temporarily remove rst-5258
    
    * rubocop
    
    * zeitwerk

--- app/controllers/concerns/starting_point_step.rb
+++ app/controllers/concerns/starting_point_step.rb
@@ -8,4 +8,11 @@ module StartingPointStep
     # if there isn't one in the session - because it's the first
     super || initialize_c100_application
   end
+
+  def update_navigation_stack
+    # The step including this concern will reset the navigation stack
+    # before re-initialising it in StepController#update_navigation_stack
+    current_c100_application.navigation_stack = []
+    super
+  end
 end
