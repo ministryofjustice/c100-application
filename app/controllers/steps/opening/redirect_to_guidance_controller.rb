36df159f5e2f9d9085204449ab5eb0b4e826e71c
Will Taylor <wrftaylor@gmail.com>
2023-04-24 16:29:49 +0100

    zeitwerk

--- app/controllers/steps/opening/redirect_to_guidance_controller.rb
+++ app/controllers/steps/opening/redirect_to_guidance_controller.rb
@@ -1,9 +1,9 @@
-# module Steps
-#   module Opening
-#     class RedirectToGuidanceController < Steps::OpeningStepController
-#       def show
-#         redirect_to 'https://apply-to-court-about-child-arrangements-c100.service.gov.uk/complete-your-application-guidance'
-#       end
-#     end
-#   end
-# end
+module Steps
+  module Opening
+    class RedirectToGuidanceController < Steps::OpeningStepController
+      #       def show
+      #         redirect_to 'https://apply-to-court-about-child-arrangements-c100.service.gov.uk/complete-your-application-guidance'
+      #       end
+    end
+  end
+end
