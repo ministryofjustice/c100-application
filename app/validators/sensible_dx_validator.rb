76c7e38e99b8e5b2060b0f68bdaa628df862124b
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2023-04-26 11:12:50 +0100

    RST-5280 Added validation to solicitors contact details, error messages and spec (#1477)

---
+++ app/validators/sensible_dx_validator.rb
@@ -0,0 +1,9 @@
+class SensibleDxValidator < ActiveModel::EachValidator
+  REGEX = /^[0-9-]+$/i.freeze
+
+  def validate_each(record, attribute, value)
+    return if value.nil? || value == ''
+
+    record.errors.add(attribute, :invalid) unless value.match(REGEX)
+  end
+end
--- spec/forms/steps/solicitor/contact_details_form_spec.rb
+++ spec/forms/steps/solicitor/contact_details_form_spec.rb
@@ -4,14 +4,17 @@ RSpec.describe Steps::Solicitor::ContactDetailsForm do
   let(:arguments) { {
     c100_application: c100_application,
     email: email,
-    phone_number: 'phone_number',
-    fax_number: 'fax_number',
-    dx_number: 'dx_number',
+    phone_number: phone_number,
+    fax_number: fax_number,
+    dx_number: dx_number,
     email_provided: email_provided,
   } }
 
   let(:email_provided) { 'yes' }
   let(:email) { 'test@example.com' }
+  let(:phone_number) { '01234123123' }
+  let(:fax_number) { '01234123123' }
+  let(:dx_number) { '123-123' }
 
   let(:c100_application) { instance_double(C100Application) }
 
@@ -41,15 +44,48 @@ RSpec.describe Steps::Solicitor::ContactDetailsForm do
           }
         end
       end
+
+      context 'phone validation' do
+        context 'phone is invalid' do
+          let(:phone_number) { 'xxx' }
+
+          it {
+            expect(subject).not_to be_valid
+            expect(subject.errors.added?(:phone_number, :invalid)).to eq(true)
+          }
+        end
+      end
+
+      context 'fax validation' do
+        context 'fax is invalid' do
+          let(:fax_number) { 'xxx' }
+
+          it {
+            expect(subject).not_to be_valid
+            expect(subject.errors.added?(:fax_number, :invalid)).to eq(true)
+          }
+        end
+      end
+
+      context 'dx validation' do
+        context 'dx is invalid' do
+          let(:dx_number) { 'xxx' }
+
+          it {
+            expect(subject).not_to be_valid
+            expect(subject.errors.added?(:dx_number, :invalid)).to eq(true)
+          }
+        end
+      end
     end
 
     it_behaves_like 'a has-one-association form',
                     association_name: :solicitor,
                     expected_attributes: {
                       email: 'test@example.com',
-                      phone_number: 'phone_number',
-                      fax_number: 'fax_number',
-                      dx_number: 'dx_number',
+                      phone_number: '01234123123',
+                      fax_number: '01234123123',
+                      dx_number: '123-123',
                       email_provided: GenericYesNo.new('yes'),
                     }
   end
