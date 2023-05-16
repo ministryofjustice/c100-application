06a2ec9576f02753bca01cb712d76a08f58bdfff
Will Taylor <wrftaylor@gmail.com>
2023-05-16 12:10:07 +0100

    reenable non-citizen path

--- spec/controllers/steps/opening/redirect_to_guidance_controller_spec.rb
+++ spec/controllers/steps/opening/redirect_to_guidance_controller_spec.rb
@@ -1,17 +1,17 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Steps::Opening::RedirectToGuidanceController, type: :controller do
-#   let(:c100_application) { instance_double(C100Application) }
+RSpec.describe Steps::Opening::RedirectToGuidanceController, type: :controller do
+  let(:c100_application) { instance_double(C100Application) }
 
-#   before do
-#     allow(controller).to receive(:current_c100_application).and_return(c100_application)
-#     allow(controller).to receive(:update_navigation_stack)
-#   end
+  before do
+    allow(controller).to receive(:current_c100_application).and_return(c100_application)
+    allow(controller).to receive(:update_navigation_stack)
+  end
 
-#   describe '#show' do
-#     it 'redirects to MyHMCTS guidance' do
-#       get :show
-#       expect(response).to redirect_to('https://apply-to-court-about-child-arrangements-c100.service.gov.uk/complete-your-application-guidance')
-#     end
-#   end
-# end
+  describe '#show' do
+    it 'redirects to MyHMCTS guidance' do
+      get :show
+      expect(response).to redirect_to('https://apply-to-court-about-child-arrangements-c100.service.gov.uk/complete-your-application-guidance')
+    end
+  end
+end
