bc6eaab21aee6d45cdf16c3938e2d3ca7a7ffdf2
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2023-06-14 15:12:23 +0100

    undo reset-opening changes (#1495)
    * undo reset-opening changes
    
    * rubocop
    
    * 00_opening feature tests and fixed PostcodeController error
    
    * Define postcode form class in file

--- spec/value_objects/application_intent_spec.rb
+++ spec/value_objects/application_intent_spec.rb
@@ -1,48 +1,48 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe ApplicationIntent do
-#   let(:value) { :foo }
-#   subject     { described_class.new(value) }
+RSpec.describe ApplicationIntent do
+  let(:value) { :foo }
+  subject     { described_class.new(value) }
 
-#   describe '.values' do
-#     it 'returns all possible values' do
-#       expect(described_class.values.map(&:to_s)).to eq(%w(new continue))
-#     end
-#   end
+  describe '.values' do
+    it 'returns all possible values' do
+      expect(described_class.values.map(&:to_s)).to eq(%w(new continue))
+    end
+  end
 
-#   describe '#new?' do
-#     describe 'when the value is `new`' do
-#       let(:value) { :new }
+  describe '#new?' do
+    describe 'when the value is `new`' do
+      let(:value) { :new }
 
-#       it 'returns true' do
-#         expect(subject.new?).to eq(true)
-#       end
-#     end
+      it 'returns true' do
+        expect(subject.new?).to eq(true)
+      end
+    end
 
-#     describe 'when the value is `continue`' do
-#       let(:value) { :continue }
+    describe 'when the value is `continue`' do
+      let(:value) { :continue }
 
-#       it 'returns false' do
-#         expect(subject.new?).to eq(false)
-#       end
-#     end
-#   end
+      it 'returns false' do
+        expect(subject.new?).to eq(false)
+      end
+    end
+  end
 
-#   describe '#continue?' do
-#     describe 'when the value is `new`' do
-#       let(:value) { :new }
+  describe '#continue?' do
+    describe 'when the value is `new`' do
+      let(:value) { :new }
 
-#       it 'returns false' do
-#         expect(subject.continue?).to eq(false)
-#       end
-#     end
+      it 'returns false' do
+        expect(subject.continue?).to eq(false)
+      end
+    end
 
-#     describe 'when the value is `continue`' do
-#       let(:value) { :continue }
+    describe 'when the value is `continue`' do
+      let(:value) { :continue }
 
-#       it 'returns true' do
-#         expect(subject.continue?).to eq(true)
-#       end
-#     end
-#   end
-# end
+      it 'returns true' do
+        expect(subject.continue?).to eq(true)
+      end
+    end
+  end
+end
