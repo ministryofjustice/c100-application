ecc96d88a5305cd7d751be9eb21bfaf99aa4e239
Will <wrftaylor@gmail.com>
2023-06-20 13:11:02 +0100

    Rst 5462 b (#1501)
    * fix circleci
    
    * fix back button error
    
    * highlight error
    
    * deploy fix
    
    * save fix
    
    * fix
    
    * fix

--- app/controllers/steps/opening/start_or_continue_controller.rb
+++ app/controllers/steps/opening/start_or_continue_controller.rb
@@ -1,7 +1,7 @@
 module Steps
   module Opening
     class StartOrContinueController < Steps::OpeningStepController
-      skip_before_action :check_c100_application_presence, :update_navigation_stack
+      skip_before_action :check_c100_application_presence
       before_action :existing_application_warning, only: [:edit], unless: -> { params[:new].present? }
       before_action :reset_c100_application_session, only: [:edit], if: -> { params[:new].present? }
       include StartingPointStep
