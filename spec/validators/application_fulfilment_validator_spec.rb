a109fed60c7437aab2848efbc97048ecd3eb8ea8
kalvinlyu <kalvin.lyu@solirius.com>
2023-06-26 15:11:49 +0100

    RST-5483 Added validation on cya for file

--- spec/validators/application_fulfilment_validator_spec.rb
+++ spec/validators/application_fulfilment_validator_spec.rb
@@ -4,6 +4,24 @@ module Test
   C100ApplicationValidatable = Struct.new(:children, :applicants, :respondents, :payment_type, :submission_type, keyword_init: true) do
     include ActiveModel::Validations
     validates_with ApplicationFulfilmentValidator
+    def document(document_key)
+      uploaded_doc = [:miam_certificate, :draft_consent_order]
+      uploaded_doc.include?(document_key)
+    end
+
+    def generate_document_validation(document_type, path, checks, invert: false)
+      lambda do |record|
+        unless record.document(document_type) || additional_checks(record, checks, invert)
+          [:files_collection_ref, :blank, path]
+        end
+      end
+    end
+
+
+    def additional_checks(record, checks, invert)
+      checks = [checks] unless checks.is_a?(Array)
+      checks.map { |check| record.send(check) }.send(invert ? :none? : :any?)
+    end
   end
 end
 
@@ -111,6 +129,41 @@ RSpec.describe ApplicationFulfilmentValidator, type: :model do
           expect(subject.errors.details[:respondents][0][:change_path]).to eq('/steps/respondent/names/')
         end
       end
+    end
+  end
+
+  context 'generate_document_validation' do
+    let(:record) { double('record') }
+    let(:validator) { ApplicationFulfilmentValidator.new }
+
+    before do
+      allow(subject).to receive(:additional_checks).and_return(true)
+    end
+
+    it 'returns the correct error variables when document is missing' do
+      allow(record).to receive(:document).with(:miam_certificate).and_return(false)
+      allow(subject).to receive(:additional_checks).and_return(false)
+
+
+      validation_send = subject.send(:generate_document_validation, :miam_certificate, '/upload/path', [])
+
+      expect(validation_send.call(record)).to eq([:files_collection_ref, :blank, '/upload/path'])
+    end
+
+    it 'returns nil when document is present' do
+      allow(record).to receive(:document).with(:miam_certificate).and_return(true)
+
+      validation_send = subject.send(:generate_document_validation, :miam_certificate, '/upload/path', [])
+
+      expect(validation_send.call(record)).to be_nil
+    end
+
+    it 'returns nil when additional checks pass' do
+      allow(record).to receive(:document).with(:miam_certificate).and_return(false)
+
+      validation_send = subject.send(:generate_document_validation, :miam_certificate, '/upload/path', [:additional_check])
+
+      expect(validation_send.call(record)).to be_nil
     end
   end
 end
