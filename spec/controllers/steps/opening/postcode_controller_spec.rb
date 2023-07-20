dfa15cdfa1f38ebd978d95d9ec2b3b4f4f295855
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2023-07-20 15:19:42 +0100

    redo opening changes (#1512)
    * redo opening changes
    
    * rubocop

--- spec/controllers/steps/opening/postcode_controller_spec.rb
+++ spec/controllers/steps/opening/postcode_controller_spec.rb
@@ -1,7 +1,7 @@
-# require 'rails_helper'
-#
-# RSpec.describe Steps::Opening::PostcodeController, type: :controller do
-#   it_behaves_like 'a starting point step controller'
-#   it_behaves_like 'an intermediate step controller', Steps::Opening::PostcodeForm, C100App::OpeningDecisionTree
-# end
-#
+require 'rails_helper'
+
+RSpec.describe Steps::Opening::PostcodeController, type: :controller do
+  it_behaves_like 'a starting point step controller'
+  it_behaves_like 'an intermediate step controller', Steps::Opening::PostcodeForm, C100App::OpeningDecisionTree
+end
+
