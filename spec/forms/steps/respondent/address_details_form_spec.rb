c0e104f1c825cfc248f1cd80d741fc1ab40cf111
kalvinlyu <kalvin.lyu@solirius.com>
2023-08-07 16:22:28 +0100

    RST-5579 Added country presence validation for respondent

--- spec/forms/steps/respondent/address_details_form_spec.rb
+++ spec/forms/steps/respondent/address_details_form_spec.rb
@@ -151,10 +151,11 @@ RSpec.describe Steps::Respondent::AddressDetailsForm do
     context 'validations on field presence when `address_unknown` is false' do
       it { should validate_presence_of(:address_line_1) }
       it { should validate_presence_of(:town) }
+      it { should validate_presence_of(:country) }
+
 
       it { should_not validate_presence_of(:address_line_2) }
       it { should_not validate_presence_of(:address_line_3) }
-      it { should_not validate_presence_of(:country) }
       it { should_not validate_presence_of(:postcode) }
     end
 
