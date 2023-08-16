683a5b1eed49a9be76d160725e108638067794e3
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2023-08-16 10:03:55 +0100

    RST-5584 Added check to cya for abuse concerns (#1518)
    * RST-5584 Test push
    
    * fix tests
    
    * RST-5584 Remove focus
    
    * RST-5584 Rubocop
    
    ---------
    
    Co-authored-by: Will Taylor <wrftaylor@gmail.com>

--- spec/presenters/summary/pdf_presenter_spec.rb
+++ spec/presenters/summary/pdf_presenter_spec.rb
@@ -3,11 +3,31 @@ require 'spec_helper'
 describe Summary::PdfPresenter do
   let(:c100_application) { C100Application.new(domestic_abuse: domestic_abuse) }
   let!(:applicant) { Applicant.new(c100_application: c100_application,
-    are_contact_details_private: are_contact_details_private) }
+                                   are_contact_details_private: are_contact_details_private) }
   let(:generator) { double('Generator') }
 
   let(:are_contact_details_private) { 'no' }
   let(:domestic_abuse) { 'no' }
+  let(:abuse_concern) {
+    instance_double(
+      AbuseConcern,
+      subject: 'applicant',
+      kind: 'emotional',
+      answer: 'no',
+      behaviour_description: 'behaviour_description',
+      behaviour_start: '2001',
+      behaviour_ongoing: 'no',
+      behaviour_stop: '2010',
+      asked_for_help: 'yes',
+      help_party: 'friend',
+      help_provided: 'yes',
+      help_description: 'help_description'
+    )
+  }
+
+  before do
+    allow(c100_application).to receive(:abuse_concerns).and_return([abuse_concern])
+  end
 
   subject { described_class.new(c100_application, generator) }
 
@@ -24,8 +44,43 @@ describe Summary::PdfPresenter do
       subject.generate
     end
 
-    context 'when C1A is triggered' do
+    context 'when C1A is triggered with has_safety_concerns' do
       let(:domestic_abuse) { 'yes' }
+
+      it 'generates the C100 form and the C1A' do
+        expect(generator).to receive(:generate).with(
+          an_instance_of(Summary::C100Form), copies: 1
+        )
+
+        expect(generator).to receive(:generate).with(
+          an_instance_of(Summary::BlankPage), copies: 1
+        )
+
+        expect(generator).to receive(:generate).with(
+          an_instance_of(Summary::C1aForm), copies: 1
+        )
+
+        subject.generate
+      end
+    end
+
+    context 'when C1A is triggered with abuse_concerns' do
+      let(:abuse_concern) {
+        instance_double(
+          AbuseConcern,
+          subject: 'applicant',
+          kind: 'emotional',
+          answer: 'yes',
+          behaviour_description: 'behaviour_description',
+          behaviour_start: '2001',
+          behaviour_ongoing: 'no',
+          behaviour_stop: '2010',
+          asked_for_help: 'yes',
+          help_party: 'friend',
+          help_provided: 'yes',
+          help_description: 'help_description'
+        )
+      }
 
       it 'generates the C100 form and the C1A' do
         expect(generator).to receive(:generate).with(
@@ -48,7 +103,7 @@ describe Summary::PdfPresenter do
       let(:are_contact_details_private) { 'yes' }
       before do
         Applicant.create(c100_application: c100_application,
-          are_contact_details_private: are_contact_details_private)
+                         are_contact_details_private: are_contact_details_private)
       end
 
       after do
@@ -89,7 +144,7 @@ describe Summary::PdfPresenter do
         before do
           expect(generator).to receive(:has_forms_data?).and_return(false)
           Applicant.create(c100_application: c100_application,
-            are_contact_details_private: are_contact_details_private)
+                           are_contact_details_private: are_contact_details_private)
         end
 
         after do
