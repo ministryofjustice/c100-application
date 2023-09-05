4c4c58cf3c82fda4a72a3c0cf5a7035a9d029a23
Will Taylor <wrftaylor@gmail.com>
2023-09-05 21:16:38 +0100

    standardize deploy

--- app/services/uploader/api_client.rb
+++ app/services/uploader/api_client.rb
@@ -1,23 +1,23 @@
 class Uploader
   class ApiClient
     def initialize(*_args)
-      @client = if Rails.env.production?
-                  Aws::S3::Client.new(
+      #@client = #if Rails.env.production?
+                @client = Aws::S3::Client.new(
                     region: ENV["AWS_S3_REGION"],
                     credentials: Aws::AssumeRoleWebIdentityCredentials.new(
                       role_arn: ENV["AWS_ROLE_ARN"],
                       web_identity_token_file: ENV["AWS_WEB_IDENTITY_TOKEN_FILE"]
                     )
                   )
-                else
-                  Aws::S3::Client.new(
-                    region: ENV["AWS_S3_REGION"],
-                    credentials: Aws::Credentials.new(
-                      ENV["AWS_S3_ACCESS_KEY_ID"],
-                      ENV["AWS_S3_SECRET_ACCESS_KEY"]
-                    )
-                  )
-                end
+                # else
+                #   Aws::S3::Client.new(
+                #     region: ENV["AWS_S3_REGION"],
+                #     credentials: Aws::Credentials.new(
+                #       ENV["AWS_S3_ACCESS_KEY_ID"],
+                #       ENV["AWS_S3_SECRET_ACCESS_KEY"]
+                #     )
+                #   )
+                # end
     end
 
     private
