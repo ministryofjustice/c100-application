8da54f434065f9ce9045ef9a3c43f696259bb07b
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2023-09-20 15:46:10 +0100

    s3 to use old credentials (#1526)
    * s3 to use old credentials
    
    * update .env

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,11 +1,20 @@
 require 'aws-sdk-core'
 
 if ENV["IS_DOCKER"].blank?
-  Aws.config.update(
-    region: ENV["AWS_S3_REGION"],
-    credentials: Aws::AssumeRoleWebIdentityCredentials.new(
-      role_arn: ENV["AWS_ROLE_ARN"],
-      web_identity_token_file: ENV["AWS_WEB_IDENTITY_TOKEN_FILE"]
+  if ENV["AWS_S3_USE_PERMANENT_CREDENTIALS"].present?
+    Aws.config.update(
+      region: ENV["AWS_S3_REGION"],
+      credentials: Aws::Credentials.new(
+        ENV["AWS_S3_ACCESS_KEY_ID"],
+        ENV["AWS_S3_SECRET_ACCESS_KEY"])
     )
-  )
+  else
+    Aws.config.update(
+      region: ENV["AWS_S3_REGION"],
+      credentials: Aws::AssumeRoleWebIdentityCredentials.new(
+        role_arn: ENV["AWS_ROLE_ARN"],
+        web_identity_token_file: ENV["AWS_WEB_IDENTITY_TOKEN_FILE"]
+      )
+    )
+  end
 end
