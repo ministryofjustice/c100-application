811cd2be9a6c0c943f6a8a07f579b6bf0d6b8232
kalvinlyu <kalvin.lyu@solirius.com>
2024-03-22 16:07:16 +0000

    rename prl_change module

--- app/controllers/steps/opening/my_hmcts_create_account_controller.rb
+++ app/controllers/steps/opening/my_hmcts_create_account_controller.rb
@@ -1,7 +1,7 @@
 module Steps
   module Opening
     class MyHmctsCreateAccountController < Steps::OpeningStepController
-      skip_before_action :check_c100_application_presence, :update_navigation_stack, if: -> { PRLChange.changes_apply? }
+      skip_before_action :check_c100_application_presence, :update_navigation_stack, if: -> { PrlChange.changes_apply? }
 
       def show
         redirect_to 'https://www.gov.uk/guidance/myhmcts-online-case-management-for-legal-professionals#create-a-myhmcts-account-for-your-organisation',
--- app/controllers/steps/opening_controller.rb
+++ app/controllers/steps/opening_controller.rb
@@ -1,7 +1,7 @@
 module Steps
   class OpeningController < ApplicationController
     def root
-      if PRLChange.changes_apply?
+      if PrlChange.changes_apply?
         redirect_to steps_opening_start_or_continue_path(new: params[:new])
       else
         redirect_to steps_opening_start_path(new: params[:new])
