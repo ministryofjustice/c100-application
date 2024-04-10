8721b39b309a1c343e44bccd69e2ec16c80563c7
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2024-04-10 11:31:01 +0100

    Update cya to show no files applicable and allow application restart (#1563)
    * Update cya to show no files applicable and allow application restart
    
    * allow file to be shown when no exemptions and hide exemption reasons when nil (#1564)
    
    * Update s3_web_identity_token_file_placeholder.txt
    
    * try new token
    
    * try new token location and add fix typo

--- app/controllers/steps/opening/start_or_continue_controller.rb
+++ app/controllers/steps/opening/start_or_continue_controller.rb
@@ -1,21 +1,26 @@
 module Steps
   module Opening
     class StartOrContinueController < Steps::OpeningStepController
-      skip_before_action :check_c100_application_presence, if: -> { PrlChange.changes_apply? }
+      skip_before_action :check_c100_application_presence, :check_application_not_completed, if: -> { PrlChange.changes_apply? }
       skip_before_action :update_navigation_stack, if: -> { PrlChange.changes_apply? }
-      before_action :existing_application_warning, only: [:edit], unless: lambda {
-                                                                            params[:new].present? && !PrlChange.changes_apply?
-                                                                          }
-      before_action :reset_c100_application_session, only: [:edit], if: lambda {
-                                                                          params[:new].present? && PrlChange.changes_apply?
-                                                                        }
-
+      before_action :existing_application_warning, only: [:edit], unless: :restarting
+      before_action :reset_c100_application_session, only: [:edit], if: :is_restarting
       def edit
         @form_object = StartOrContinueForm.build(current_c100_application)
       end
 
       def update
         update_and_advance(StartOrContinueForm)
+      end
+
+      private
+
+      def restarting
+        is_attempting_restart? && !PrlChange.changes_apply?
+      end
+
+      def is_restarting
+        is_attempting_restart? && PrlChange.changes_apply?
       end
     end
   end
