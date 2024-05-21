0b8d1cf35634e18f2288eb03938b642d1f118a26
kalvinlyu <kalvin.lyu@solirius.com>
2024-05-21 15:01:11 +0100

    fix opening changes for prl

--- app/controllers/steps/opening_controller.rb
+++ app/controllers/steps/opening_controller.rb
@@ -4,7 +4,7 @@ module Steps
       if PrlChange.changes_apply?
         redirect_to steps_opening_start_or_continue_path(new: params[:new], change: params[:change])
       else
-        redirect_to steps_opening_start_path(new: params[:new])
+        redirect_to steps_opening_start_path(new: params[:new], change: params[:change])
       end
     end
   end
