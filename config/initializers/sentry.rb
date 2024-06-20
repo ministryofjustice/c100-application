422f64074c5d7063c85447a387a6eed70df1438c
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2024-06-20 15:22:51 +0100

    RST-6738 Reduce Sentry transaction rate and prevent http error from puma (#1629)

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,6 @@
+EXCLUDE_PATHS = ['/ping', '/ping.json', '/health', '/health.json'].freeze
+
 # Will use SENTRY_DSN environment variable if set
-#
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
 
@@ -8,5 +9,21 @@ Sentry.init do |config|
   # Convert to regex, otherwise Sentry will threat them as words
   # https://github.com/getsentry/raven-ruby/blob/master/lib/raven/processor/sanitizedata.rb
   # config.sanitize_fields = Rails.application.config.filter_parameters.map { |w| "#{w}+" }
+
+  config.traces_sampler = lambda do |sampling_context|
+    transaction_context = sampling_context[:transaction_context]
+    transaction_name = transaction_context[:name]
+
+    transaction_name.in?(EXCLUDE_PATHS) ? 0.0 : 0.01
+  end
+
+  config.before_send = lambda do |event, hint|
+    # NOTE: hint[:exception] would be a String if you use async callback
+    if hint[:exception].is_a?(Puma::HttpParserError)
+      nil
+    else
+      event
+    end
+  end
 end
 
