1dfcd4fa2648d5376adcf2384acd6b8f0a2f0219
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2024-07-25 16:19:49 +0100

    RST-6730 remove centralised address (#1634)
    * RST-6730 remove centralised address
    
    * update token

--- app/models/concerns/court_contact_details.rb
+++ app/models/concerns/court_contact_details.rb
@@ -6,13 +6,6 @@ module CourtContactDetails
   FACT_COURT_BASE_URL = 'https://www.find-court-tribunal.service.gov.uk/courts/'.freeze
 
   CENTRAL_HUB_EMAIL = 'C100applications@justice.gov.uk'.freeze
-  CENTRAL_HUB_ADDRESS = [
-    'C100 Applications',
-    'PO Box 1792',
-    'Southampton',
-    'SO15 9GG',
-    'DX: 135986 Southampton 32',
-  ].freeze
 
   def centralised?
     centralised_slugs.include?(slug)
@@ -23,9 +16,6 @@ module CourtContactDetails
   end
 
   def full_address
-    return CENTRAL_HUB_ADDRESS if centralised?
-
-    # If the court is not yet centralised we default to its postal address
     [
       name,
       address.fetch_values(
