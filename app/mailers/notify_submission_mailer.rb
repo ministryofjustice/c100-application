543cba46900c8cca0a0204afcb320dd17df79139
kalvinlyu <kalvin.lyu@solirius.com>
2024-08-29 15:49:16 +0100

    RST-6845a notify boolean fix

--- app/mailers/notify_submission_mailer.rb
+++ app/mailers/notify_submission_mailer.rb
@@ -28,7 +28,7 @@ class NotifySubmissionMailer < NotifyMailer
     set_personalisation(
       shared_personalisation.merge(
         urgent: @c100_application.mark_as_urgent? ? 'yes' : 'no',
-        safety_concerns: @c100_application.has_safety_concerns?,
+        safety_concerns: notify_boolean(@c100_application.has_safety_concerns?),
         c8_included: @c100_application.confidentiality_enabled? ? 'yes' : 'no',
         link_to_c8_pdf: prepare_upload(@documents[:c8_form]),
         link_to_pdf: prepare_upload(@documents[:bundle]),
