e4a11163f0f34b26b268282586c6c11e0c6e1a2b
sgmselli <sellings.matthew@gmail.com>
2024-09-06 15:16:31 +0100

    Resolving "DEPRECATION WARNING: Defining enums with keyword arguments is deprecated and will be removed"

--- app/models/c100_application.rb
+++ app/models/c100_application.rb
@@ -3,16 +3,16 @@ class C100Application < ApplicationRecord
   include ApplicationReference
   include CourtRedirections
 
-  enum status: {
-    screening: 0,
-    in_progress: 1,
-    payment_in_progress: 8,
-    completed: 10,
+  enum :status, {
+    :screening=>0,
+    :in_progress=>1,
+    :payment_in_progress=>8,
+    :completed=>10,
   }
 
-  enum reminder_status: {
-    first_reminder_sent: 'first_reminder_sent',
-    last_reminder_sent: 'last_reminder_sent',
+  enum :reminder_status, {
+    :first_reminder_sent=>'first_reminder_sent',
+    :last_reminder_sent=>'last_reminder_sent',
   }
 
   belongs_to :user, optional: true
