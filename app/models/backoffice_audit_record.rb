a43e9a9a9d1082c69ccf6e7f78c315e96dec8817
sgmselli <sellings.matthew@gmail.com>
2024-09-06 15:20:12 +0100

    Resolving "DEPRECATION WARNING: Defining enums with keyword arguments is deprecated and will be removed"

--- app/models/backoffice_audit_record.rb
+++ app/models/backoffice_audit_record.rb
@@ -2,14 +2,14 @@ class BackofficeAuditRecord < ApplicationRecord
   # Do not remove or rename any of these to ensure the audit trail
   # is maintained. Only add new actions when needed.
   #
-  enum action: {
-    login: 'login',
-    logout: 'logout',
-    forbidden: 'forbidden',
-    application_lookup: 'application_lookup',
-    application_completed: 'application_completed',
-    email_lookup: 'email_lookup',
-    resend_email: 'resend_email',
+  enum :action, {
+    :login=>'login',
+    :logout=>'logout',
+    :forbidden=>'forbidden',
+    :application_lookup=>'application_lookup',
+    :application_completed=>'application_completed',
+    :email_lookup=>'email_lookup',
+    :resend_email=>'resend_email',
   }
 
   def self.log!(author:, action:, details: {})
