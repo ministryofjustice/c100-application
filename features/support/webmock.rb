90b934d1118b7234940683e626fe9bf43adac27e
Petr Zaparka <petr@zaparka.cz>
2024-09-09 15:39:04 +0100

    S3 feature test fix

---
+++ features/support/webmock.rb
@@ -0,0 +1,10 @@
+# require 'webmock'
+# # rubocop:disable Style/MixinUsage
+# include WebMock::API
+# # rubocop:enable Style/MixinUsage
+# # require_relative 'capybara_driver_helper'
+
+# # selenium_url = URI.parse ENV.fetch('SELENIUM_URL', 'http://localhost:4444/wd/hub')
+# # app_host_url = URI.parse Capybara.app_host
+
+# WebMock.disable_net_connect!(allow_localhost: true)
