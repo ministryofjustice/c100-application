6777ce611229851ceb6f4728ffb556d2ec42d862
Petr Zaparka <petr@zaparka.cz>
2024-09-10 16:18:40 +0100

    Another CI fix version

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -1,17 +1,33 @@
 Given("I am on safety concern page") do
+  expect(page).to have_text 'What kind of application do you want to make?'
   choose('Consent order', visible: :all)
   click_button "Continue"
-  expect(page).to have_text "Upload the draft of your consent order"
-  attach_file(Rails.root + 'features/support/sample_file/image.jpg')
-  click_button "Continue"
+  upload_file
   expect(page).to have_text "You do not have to attend a MIAM"
   click_link("Continue")
   expect(page).to have_text "Does this application concern a child who is the subject of separate ongoing emergency proceedings, care proceedings or supervision proceedings (or is already the subject of an emergency, care or supervision order)?"
   choose('No', visible: :all)
-  click_button "Continue"
-  expect(page).to have_text "Safety concerns"
-  click_link("Continue")
+  click_button("Continue", wait: true)
+  step_safety_concern
   expect(page).to have_text "Are the children at risk of being abducted?"
+rescue Selenium::WebDriver::Error::UnknownError => e
+  puts 'failed safety concern'
+end
+
+def step_safety_concern
+  expect(page).to have_text "Safety concerns"
+  puts '5'
+  click_link("Continue", wait: true)
+rescue Selenium::WebDriver::Error::UnknownError => e
+  sleep 1
+  expect(page).to have_text "Safety concerns"
+  click_link("Continue", wait: true)
+end
+
+def upload_file
+  expect(page).to have_text "Upload the draft of your consent order"
+  attach_file(Rails.root + 'features/support/sample_file/image.jpg')
+  click_button "Continue"
 end
 
 Then("I create an account") do
