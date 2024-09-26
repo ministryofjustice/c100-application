4bc5d6e551ffa23033ad0bf6d36ee98c83cd8054
DanielleKushnir <124261069+DanielleKushnir@users.noreply.github.com>
2024-09-26 15:14:44 +0100

    RST-6895 Daily task for submitted applications (#1671)
    RST-6895 Daily task for submitted applications

---
+++ app/services/reports/submitted_applications_report.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Reports
+  # :nocov:
+  class SubmittedApplicationsReport
+    require 'csv'
+
+    class << self
+      def run
+        return unless ENV.key?('PAYMENT_TYPE_REPORT_EMAIL')
+
+        Rails.logger.info "Sending submitted applications report"
+
+        begin
+          retries ||= 0
+          Rails.logger.info "try ##{retries}"
+          report_csv
+          ReportsMailer.submitted_applications_report(
+            report_csv,
+            to_address: ENV['PAYMENT_TYPE_REPORT_EMAIL'],
+          ).deliver_later
+        rescue PG::ConnectionBad => e
+          retry if (retries += 1) < 3
+          Sentry.capture_exception(e)
+          raise
+        end
+      end
+
+      def report_data
+        from = 1.day.ago.beginning_of_day
+        to = Date.today.beginning_of_day
+        CompletedApplicationsAudit
+          .where(completed_at: from...to)
+          .pluck(:reference_code, :completed_at)
+      end
+
+      def report_csv
+        CSV.generate do |csv|
+          csv << ['Date', Date.today]
+          csv << ['Reference Number', 'Date/Time Submitted']
+          report_data.each { |row| csv << row }
+          csv << ['Total Applications', report_data.count]
+        end
+      end
+    end
+  end
+end
