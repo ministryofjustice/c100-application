9321e8655b93c426db84778594698574e7d7f1de
sgmselli <sellings.matthew@gmail.com>
2024-11-12 11:47:47 +0000

    refactor code for config.load_defaults update from 5.2 to 7.1

--- app/controllers/download_tokens_controller.rb
+++ app/controllers/download_tokens_controller.rb
@@ -4,7 +4,7 @@ class DownloadTokensController < ApplicationController
   def show
     download_token = DownloadToken.find_by_token params[:token]
 
-    redirect_to file_not_found_errors_path and return unless download_token
-    redirect_to download_token.url
+    redirect_to file_not_found_errors_path, allow_other_host: true and return unless download_token
+    redirect_to download_token.url, allow_other_host: true
   end
 end
