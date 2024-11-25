ccb14e18e52ae0d36b7f3f35c15a12f8dc8b2f05
Petr Zaparka <petr@zaparka.cz>
2024-11-25 14:07:51 +0000

    Usr redis for session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -10,4 +10,9 @@
 # As we can't rely only on JS, there is also a request-based check to expire
 # the session if the cookie is too old (see `/concerns/security_handling.rb`).
 #
-Rails.application.config.session_store :cookie_store, key: '_c100_application_session', secure: Rails.env.production?
+# Rails.application.config.session_store :mem_cache_store, key: '_c100_application_session', secure: Rails.env.production?
+redis_url = { url: ENV["REDIS_URL"] || "redis://localhost:6379/1" }
+Rails.application.config.session_store :redis_store, url: redis_url,
+                                          key: '_c100_application_session',
+                                          secure: Rails.env.production?,
+                                          expire_after: 30.days
