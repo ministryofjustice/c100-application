7aa2e17133b35420986dd4a44cf9b49246223dfa
Matthew Sellings <99349293+sgmselli@users.noreply.github.com>
2024-12-04 09:50:00 +0000

    RST-6963 & 6975 Added refuge page to Other Parties (#1697)
    Added refuge page to Other Parties & fixed bug with auto filling radio buttons

--- app/controllers/steps/other_party/privacy_preferences_controller.rb
+++ app/controllers/steps/other_party/privacy_preferences_controller.rb
@@ -3,23 +3,15 @@ module Steps
     class PrivacyPreferencesController < Steps::OtherPartyStepController
       def edit
         @form_object = PrivacyPreferencesForm.build(
-          relationship_record, c100_application: current_c100_application
+          current_record, c100_application: current_c100_application
         )
       end
 
       def update
         update_and_advance(
           PrivacyPreferencesForm,
-          record: relationship_record,
+          record: current_record,
           as: :privacy_preferences
-        )
-      end
-
-      private
-
-      def relationship_record
-        current_c100_application.relationships.find_or_initialize_by(
-          person: current_record
         )
       end
     end
