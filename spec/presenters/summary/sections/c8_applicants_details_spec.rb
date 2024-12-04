5724a96697aad4fec341e5eb3c072de21e693b26
DanielleKushnir <124261069+DanielleKushnir@users.noreply.github.com>
2024-12-04 17:07:28 +0000

    RST-7003 All contact details confidential if resident in a refuge (#1701)
    RST-7003 - Put all contact details in C8 if applicant in refuge

--- spec/presenters/summary/sections/c8_applicants_details_spec.rb
+++ spec/presenters/summary/sections/c8_applicants_details_spec.rb
@@ -12,16 +12,15 @@ module Summary
 
     let(:applicant) {
       instance_double(Applicant,
-        full_name: 'fullname',
-        residence_history: 'history',
-        home_phone: 'home_phone',
-        mobile_provided: mobile_provided,
-        mobile_not_provided_reason: mobile_not_provided_reason,
-        mobile_phone: 'mobile_phone',
-        email: 'email',
-        voicemail_consent: 'yes',
-        contact_details_private: contact_details_private,
-        refuge: 'yes'
+                      full_name: 'fullname',
+                      residence_history: 'history',
+                      home_phone: 'home_phone',
+                      mobile_provided: mobile_provided,
+                      mobile_not_provided_reason: mobile_not_provided_reason,
+                      mobile_phone: 'mobile_phone',
+                      email: 'email',
+                      voicemail_consent: 'yes',
+                      contact_details_private: contact_details_private
       )
     }
 
@@ -37,6 +36,7 @@ module Summary
       allow(applicant).to receive(:mobile_private?).and_return(contact_details_private.include?('mobile'))
       allow(applicant).to receive(:home_phone_private?).and_return(contact_details_private.include?('home_phone'))
       allow(applicant).to receive(:address_private?).and_return(contact_details_private.include?('address'))
+      allow(applicant).to receive(:refuge).and_return('yes')
     end
 
     subject { described_class.new(c100_application) }
@@ -107,31 +107,97 @@ module Summary
         expect(answers[10].name).to eq(:row_blank_space)
       end
 
+      it 'marks all private contact details correctly' do
+        expect(applicant.email_private?).to be(true)
+        expect(applicant.home_phone_private?).to be(true)
+        expect(applicant.mobile_private?).to be(true)
+        expect(applicant.address_private?).to be(true)
+      end
+
       context "when no mobile and a reason given" do
+        before do
+          allow(applicant).to receive(:refuge).and_return('no')
+        end
+
         let(:mobile_not_provided_reason) { "No phone" }
         let(:mobile_provided) { 'no' }
+
         it "shows the reason" do
           expect(answers[5].value).to eq('No phone')
         end
       end
 
       context 'only one marked as private' do
+        before do
+          allow(applicant).to receive(:refuge).and_return('no')
+        end
+
         let(:applicant) {
           instance_double(Applicant,
-            full_name: 'fullname',
-            residence_history: 'history',
-            home_phone: 'home_phone',
-            mobile_phone: 'mobile_phone',
-            email: 'email',
-            voicemail_consent: 'yes',
-            contact_details_private: contact_details_private
+                          full_name: 'fullname',
+                          residence_history: 'history',
+                          home_phone: 'home_phone',
+                          mobile_phone: 'mobile_phone',
+                          email: 'email',
+                          voicemail_consent: 'yes',
+                          contact_details_private: contact_details_private
           )
         }
         let(:contact_details_private) { [] }
         it 'has the correct number of rows' do
           expect(answers.count).to eq(0)
         end
+      end
 
+      context 'when refuge is no, only selected private details should be private' do
+        let(:contact_details_private) { ['email', 'home_phone'] }
+
+        before do
+          allow(applicant).to receive(:refuge).and_return('no')
+        end
+
+        it 'marks only the selected private details as private' do
+          expect(applicant.email_private?).to be(true)
+          expect(applicant.home_phone_private?).to be(true)
+          expect(applicant.mobile_private?).to be(false)
+          expect(applicant.address_private?).to be(false)
+        end
+
+        it 'has the correct number of rows' do
+          expect(answers.count).to eq(8)
+        end
+
+        it 'returns the correct content for answers' do
+          expect(answers[0]).to be_an_instance_of(Separator)
+          expect(answers[0].title).to eq('c8_applicants_details_index_title')
+          expect(answers[0].i18n_opts).to eq({index: 1})
+
+          expect(answers[1]).to be_an_instance_of(FreeTextAnswer)
+          expect(answers[1].question).to eq(:person_full_name)
+          expect(answers[1].value).to eq('fullname')
+
+          expect(answers[2]).to be_an_instance_of(FreeTextAnswer)
+          expect(answers[2].question).to eq(:person_email)
+          expect(answers[2].value).to eq('email')
+
+          expect(answers[3]).to be_an_instance_of(FreeTextAnswer)
+          expect(answers[3].question).to eq(:person_home_phone)
+          expect(answers[3].value).to eq('home_phone')
+
+          expect(answers[4]).to be_an_instance_of(Answer)
+          expect(answers[4].question).to eq(:person_voicemail_consent)
+          expect(answers[4].value).to eq('yes')
+
+          expect(answers[5]).to be_an_instance_of(Answer)
+          expect(answers[5].question).to eq(:refuge)
+          expect(answers[5].value).to eq('no')
+
+          expect(answers[6]).to be_an_instance_of(Partial)
+          expect(answers[6].name).to eq(:row_blank_space)
+
+          expect(answers[7]).to be_an_instance_of(Partial)
+          expect(answers[7].name).to eq(:row_blank_space)
+        end
       end
     end
   end
