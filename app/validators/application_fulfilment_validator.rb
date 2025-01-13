c0fa3c47f6f1f09268dd60a9dc531db54937081c
DanielleKushnir <daniellekushnir22@gmail.com>
2025-01-13 14:40:01 +0000

    RST-7081 - Corrected MIAM validator

--- app/validators/application_fulfilment_validator.rb
+++ app/validators/application_fulfilment_validator.rb
@@ -21,7 +21,8 @@ class ApplicationFulfilmentValidator < ActiveModel::Validator
       generate_validation(:applicants, edit_steps_applicant_names_path(id: '')),
       generate_validation(:respondents, edit_steps_respondent_names_path(id: '')),
       generate_validation(:payment_type, edit_steps_application_payment_path, :present?),
-      generate_miam_validation,
+      generate_miam_validation_reasons,
+      generate_miam_validation_details,
       generate_document_validation(:miam_certificate, edit_steps_miam_certification_upload_path,
                                    [:consent_order?, :child_protection_cases?, :miam_exemption_claim?]),
       generate_document_validation(:draft_consent_order, edit_steps_opening_consent_order_upload_path,
@@ -45,13 +46,23 @@ class ApplicationFulfilmentValidator < ActiveModel::Validator
     ->(record) { [:orders, :blank, edit_steps_petition_orders_path] unless record.has_petition_orders? }
   end
 
-  def generate_miam_validation
+  def generate_miam_validation_reasons
     lambda { |record|
       [:attach_evidence, :blank,
        edit_steps_miam_exemptions_exemption_reasons_path] unless record.attach_evidence.present? ||
                                                                  record.consent_order == 'yes' ||
-                                                                 has_misc_skip_exemptions?(record) ||
                                                                  has_other_skip_exemptions?(record) ||
+                                                                 record.document(:miam_certificate) ||
+                                                                 record.child_protection_cases == 'yes'
+    }
+  end
+
+  def generate_miam_validation_details
+    lambda { |record|
+      [:attach_evidence, :blank,
+       edit_steps_miam_exemptions_exemption_details_path] unless record.attach_evidence.present? ||
+                                                                 record.consent_order == 'yes' ||
+                                                                 has_misc_skip_exemptions?(record) ||
                                                                  record.document(:miam_certificate) ||
                                                                  record.child_protection_cases == 'yes'
     }
@@ -60,25 +71,27 @@ class ApplicationFulfilmentValidator < ActiveModel::Validator
   def has_misc_skip_exemptions?(record)
     return false unless record&.miam_exemption&.misc
     exemptions = record.miam_exemption.misc
-    return true if %w[misc_access misc_access2 misc_access3].any? { |misc| exemptions.include? misc }
-    false
+    return false if %w[misc_access misc_access2 misc_access3].any? { |misc| exemptions.include? misc }
+    true
   end
 
-  # rubocop:disable Metrics/PerceivedComplexity
   def has_other_skip_exemptions?(record)
     return false unless record&.miam_exemption&.misc
 
     exemptions = record.miam_exemption.misc
-    groups = [:protection, :urgency, :adr]
 
-    return true if %w[misc_access misc_access2 misc_access3].any? { |access| exemptions.include?(access) }
-    return true unless record.miam_exemption.domestic.include?("misc_domestic_none")
-    return true if groups.any? { |group| !record.miam_exemption.send(group).include?("misc_#{group}_none") }
-    return false if %w[misc_without_notice misc_applicant_under_age misc_access4].any? { |misc| exemptions.include?(misc) }
+    return false if record.miam_exemption.domestic.exclude?("misc_domestic_none")
+    return false if %w[misc_without_notice misc_applicant_under_age misc_access4].any? do |misc|
+      exemptions.include?(misc)
+    end && other_group_check(record)
 
     true
   end
-  # rubocop:enable Metrics/PerceivedComplexity
+
+  def other_group_check(record)
+    groups = [:protection, :urgency, :adr]
+    groups.all? { |group| record.miam_exemption.send(group).include?("misc_#{group}_none") }
+  end
 
   def additional_checks(record, checks, invert)
     checks.map { |check| record.send(check) }.send(invert ? :none? : :any?)
