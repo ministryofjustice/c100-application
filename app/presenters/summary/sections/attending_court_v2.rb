120db013d6a6bb1620614ca72a7fdd870f78aad8
DanielleKushnir <daniellekushnir22@gmail.com>
2025-01-23 09:31:32 +0000

    RST-7104 - Use no arrangement values if court arrangements not provided

--- app/presenters/summary/sections/attending_court_v2.rb
+++ app/presenters/summary/sections/attending_court_v2.rb
@@ -25,10 +25,21 @@ module Summary
       private
 
       def arrangement
-        @_arrangement ||= begin
-          arrangement = c100.court_arrangement || CourtArrangement.new
-          arrangement
-        end
+        @_arrangement ||= c100.court_arrangement || no_arrangement
+      end
+
+      def no_arrangement
+        arrangement = CourtArrangement.new
+
+        arrangement.intermediary_help ||= GenericYesNo::NO
+        arrangement.language_options ||= []
+        arrangement.language_interpreter_details ||= I18n.t('dictionary.not_needed')
+        arrangement.sign_language_interpreter_details ||= I18n.t('dictionary.not_needed')
+        arrangement.welsh_language_details ||= I18n.t('dictionary.not_needed')
+        arrangement.special_arrangements ||= I18n.t('dictionary.not_needed')
+        arrangement.special_assistance ||= I18n.t('dictionary.not_needed')
+
+        arrangement
       end
 
       def intermediary
