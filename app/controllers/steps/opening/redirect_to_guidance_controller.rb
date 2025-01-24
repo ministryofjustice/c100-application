bb7f26a888977fc125abf478481efae192378809
kalvinlyu <kalvin.lyu@solirius.com>
2025-01-24 13:45:25 +0000

    RST-7108 change guidance redirect url to prod url

--- app/controllers/steps/opening/redirect_to_guidance_controller.rb
+++ app/controllers/steps/opening/redirect_to_guidance_controller.rb
@@ -2,7 +2,7 @@ module Steps
   module Opening
     class RedirectToGuidanceController < Steps::OpeningStepController
       def show
-        redirect_to 'https://privatelaw.aat.platform.hmcts.net/complete-your-application-guidance', allow_other_host: true
+        redirect_to 'https://www.apply-to-court-about-child-arrangements-c100.service.gov.uk/complete-your-application-guidance', allow_other_host: true
       end
     end
   end
