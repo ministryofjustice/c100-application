5e16dbafc7e1f984951894de04f6ff1da437236f
sgmselli <sellings.matthew@gmail.com>
2025-01-28 10:48:06 +0000

    rails 8 update

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,7 +1,8 @@
-# Note: no need to specify the whole exact parameter name, as this works like
-# wildcards, so for example, entering here `details` will filter parameters
-# with names like `substance_abuse_details`.
-#
+# Be sure to restart your server when you modify this file.
+
+# Configure parameters to be partially matched (e.g. passw matches password) and filtered from the log file.
+# Use this to limit dissemination of sensitive information.
+# See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
   :email,
   :password,
