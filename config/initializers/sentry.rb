b0a272b729debc5474c7eeca992c60e289d4adea
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2025-04-01 09:14:32 +0100

    RST-7246 undo sentry logging (#1756)

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 ## DEV SENTRY DSN
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  # config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Add data like request headers and IP for users,
   # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
@@ -10,7 +10,7 @@ Sentry.init do |config|
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for tracing.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
+  # config.traces_sample_rate = 1.0
   # or
   config.traces_sampler = lambda do |_context|
     true
@@ -18,5 +18,5 @@ Sentry.init do |config|
   # Set profiles_sample_rate to profile 100%
   # of sampled transactions.
   # We recommend adjusting this value in production.
-  config.profiles_sample_rate = 1.0
+  # config.profiles_sample_rate = 1.0
 end
