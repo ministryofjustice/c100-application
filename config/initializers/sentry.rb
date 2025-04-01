b46219ad86d0254063c4064b91dd86811779422d
Petr Zaparka <petr@zaparka.cz>
2025-04-01 13:49:34 +0100

    Excluding ping from sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 ## DEV SENTRY DSN
+EXCLUDE_PATHS = ['/ping', '/ping.json', '/health', '/health.json'].freeze
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
   # config.breadcrumbs_logger = [:active_support_logger, :http_logger]
@@ -12,8 +13,10 @@ Sentry.init do |config|
   # We recommend adjusting this value in production.
   # config.traces_sample_rate = 1.0
   # or
-  config.traces_sampler = lambda do |_context|
-    true
+  config.traces_sampler = lambda do |sampling_context|
+    transaction_context = sampling_context[:transaction_context]
+    transaction_name = transaction_context[:name]
+    transaction_name.in?(EXCLUDE_PATHS) ? 0.0 : 0.01
   end
   # Set profiles_sample_rate to profile 100%
   # of sampled transactions.
