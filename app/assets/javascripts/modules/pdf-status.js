84f763434c1b8c19b1b2389a03b98ad26520e0fc
DanielleKushnir <daniellekushnir22@gmail.com>
2025-04-15 16:44:44 +0100

    RST-7188 - Add JS module loader

--- app/assets/javascripts/modules/pdf-status.js
+++ app/assets/javascripts/modules/pdf-status.js
@@ -1,71 +1,71 @@
-// 'use strict';
-//
-// moj.Modules.pdfStatusPolling = {
-//     init: function () {
-//         console.log('<-- pdfStatusPolling module init -->');
-//         const setups = [
-//             {
-//                 type: 'draft',
-//                 buttonId: 'draftDownloadButton',
-//                 messageId: 'draftGeneratingMessage',
-//             },
-//             {
-//                 type: 'completed',
-//                 buttonId: 'completedDownloadButton',
-//                 messageId: 'completedGeneratingMessage',
-//             },
-//         ];
-//
-//         setups.forEach(({ type, buttonId, messageId }) => {
-//             const button = document.getElementById(buttonId);
-//             const message = document.getElementById(messageId);
-//
-//             if (!button || !message) {
-//                 return;
-//             }
-//
-//             console.log('<-- elements found -->');
-//             function preventClick(e) {
-//                 e.preventDefault();
-//                 console.log('<-- prevented download click -->');
-//             }
-//
-//             function disableDownloadLink() {
-//                 console.log('<-- disabling button -->');
-//                 button.classList.add('govuk-button--disabled');
-//                 button.setAttribute('aria-disabled', 'true');
-//                 button.addEventListener('click', preventClick, true);
-//             }
-//
-//             function enableDownloadLink() {
-//                 console.log('<-- enabling button -->');
-//                 button.classList.remove('govuk-button--disabled');
-//                 button.removeAttribute('aria-disabled');
-//                 button.removeEventListener('click', preventClick, true);
-//             }
-//
-//             function checkJobStatus(intervalId) {
-//                 console.log('<-- polling job status -->');
-//                 fetch(`/steps/completion/summary/check_pdf_status?type=${type}`)
-//                     .then((response) => response.json())
-//                     .then((data) => {
-//                         console.log('<-- received response from job -->');
-//                         if (data.job_completed) {
-//                             console.log('<-- PDF ready -->');
-//                             enableDownloadLink();
-//                             message.classList.add('hidden');
-//                             clearInterval(intervalId);
-//                         } else {
-//                             console.log('<-- waiting for PDF -->');
-//                             disableDownloadLink();
-//                             message.classList.remove('hidden');
-//                         }
-//                     })
-//             }
-//
-//             disableDownloadLink();
-//             const intervalId = setInterval(() => checkJobStatus(intervalId), 2000);
-//             checkJobStatus(intervalId);
-//         });
-//     },
-// };
+'use strict';
+
+window.moj.Modules.PdfStatus = {
+    init: function () {
+        console.log('[PdfStatus] running');
+
+        const setups = [
+            {
+                type: 'draft',
+                buttonId: 'draftDownloadButton',
+                messageId: 'draftGeneratingMessage',
+            },
+            {
+                type: 'completed',
+                buttonId: 'completedDownloadButton',
+                messageId: 'completedGeneratingMessage',
+            },
+        ];
+
+        setups.forEach(({ type, buttonId, messageId }) => {
+            const button = document.getElementById(buttonId);
+            const message = document.getElementById(messageId);
+
+            if (!button || !message) {
+                console.log(`Missing button/message for ${type}`);
+                return;
+            }
+
+            console.log(`Found elements for ${type}`);
+
+            function preventClick(e) {
+                e.preventDefault();
+            }
+
+            function disableDownloadLink() {
+                console.log(`Disabling ${type}`);
+                button.classList.add('govuk-button--disabled');
+                button.setAttribute('aria-disabled', 'true');
+                button.addEventListener('click', preventClick, true);
+            }
+
+            function enableDownloadLink() {
+                console.log(`Enabling ${type}`);
+                button.classList.remove('govuk-button--disabled');
+                button.removeAttribute('aria-disabled');
+                button.removeEventListener('click', preventClick, true);
+            }
+
+            function checkJobStatus(intervalId) {
+                console.log(`Polling for ${type}`);
+                fetch(`/steps/completion/summary/check_pdf_status?type=${type}`)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        console.log(`Response for ${type}`, data);
+                        if (data.job_completed) {
+                            enableDownloadLink();
+                            message.classList.add('hidden');
+                            clearInterval(intervalId);
+                        } else {
+                            disableDownloadLink();
+                            message.classList.remove('hidden');
+                        }
+                    });
+            }
+
+            disableDownloadLink();
+            const intervalId = setInterval(() => checkJobStatus(intervalId), 2000);
+            checkJobStatus(intervalId);
+        });
+    },
+};
