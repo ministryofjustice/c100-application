63ae82cf2b41985b24394cbec535472931e84f96
DanielleKushnir <daniellekushnir22@gmail.com>
2025-04-16 16:30:25 +0100

    RST-7188 - Added debugging statements

--- app/jobs/pdf_generation_job.rb
+++ app/jobs/pdf_generation_job.rb
@@ -2,12 +2,15 @@ class PdfGenerationJob < ApplicationJob
   queue_as :default
 
   def perform(application_id:, type:)
+    Rails.logger.info "PdfGenerationJob started for #{application_id} / #{type}"
+
     application = C100Application.find(application_id).reload
     presenter = Summary::PdfPresenter.new(application)
     presenter.generate
 
     pdf_data = presenter.to_pdf
     key = "C100_Application_#{application_id}_#{type}"
+    Rails.logger.info "Writing cache key: C100_Application_#{application_id}_#{type}"
     Rails.cache.write(key, pdf_data, expires_in: 15.minutes)
   end
 end
