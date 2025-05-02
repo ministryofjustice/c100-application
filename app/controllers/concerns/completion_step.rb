4ae6e0e8001d70a9052ab8b5f8494d0787b7c392
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2025-05-02 17:27:16 +0100

    Revert "RST-7188 - Improve PDF Generation Performance (#1759)" (#1764)
    This reverts commit 8381d2e239e86ea616f9cdbd5c505936e4f9064c.

--- app/controllers/concerns/completion_step.rb
+++ app/controllers/concerns/completion_step.rb
@@ -3,7 +3,6 @@ module CompletionStep
 
   included do
     before_action :check_application_is_completed
-    before_action :generate_pdf
   end
 
   def show
@@ -17,10 +16,5 @@ module CompletionStep
     return if current_c100_application.completed?
 
     redirect_to steps_opening_warning_path, allow_other_host: true
-  end
-
-  def generate_pdf
-    Rails.cache.delete("C100_Application_#{current_c100_application.id}_completed")
-    PdfGenerationJob.perform_later(application_id: current_c100_application.id, type: 'completed')
   end
 end
