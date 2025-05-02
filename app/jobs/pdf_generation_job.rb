7f053fef77e1e0511a89d61cde3b7dd20e5ba50b
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2025-05-02 17:28:27 +0100

    Revert "Revert "RST-7188 - Improve PDF Generation Performance (#1759)" (#1764)"
    This reverts commit d5f1aaaa1f2157dc60742260c194d3b69c9e39e2.

---
+++ app/jobs/pdf_generation_job.rb
@@ -0,0 +1,13 @@
+class PdfGenerationJob < ApplicationJob
+  queue_as :default
+
+  def perform(application_id:, type:)
+    application = C100Application.find(application_id).reload
+    presenter = Summary::PdfPresenter.new(application)
+    presenter.generate
+
+    pdf_data = presenter.to_pdf
+    key = "C100_Application_#{application_id}_#{type}"
+    Rails.cache.write(key, pdf_data, expires_in: 15.minutes)
+  end
+end
