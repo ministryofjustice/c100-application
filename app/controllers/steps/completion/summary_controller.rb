e877b7135891016abc2db12d5fba7f13587b10e3
Petr Zaparka <petr@zaparka.cz>
2025-06-09 10:24:03 +0100

    RST-7334 - Show draft PDF content on HTML page (#1774)
    RST-7334 - Show draft PDF content on HTML page

--- app/controllers/steps/completion/summary_controller.rb
+++ app/controllers/steps/completion/summary_controller.rb
@@ -2,14 +2,23 @@ module Steps
   module Completion
     class SummaryController < Steps::CompletionStepController
       def show
+        presenter = Summary::PdfPresenter.new(current_c100_application)
+
         respond_to do |format|
           format.pdf do
-            presenter = Summary::PdfPresenter.new(current_c100_application)
             presenter.generate
-
             # Will render the template defined in `BasePdfForm#template`
             # i.e. `steps/completion/summary/show.pdf.erb`
             send_data(presenter.to_pdf, filename: presenter.filename)
+          end
+
+          format.html do
+            presenter.generate(mode: :html)
+            html = presenter.collected_forms.map do |form|
+              C100App::PdfGenerator.new.send(:render, form)
+            end.join.html_safe
+
+            render html: html
           end
         end
       end
--- app/presenters/summary/pdf_presenter.rb
+++ app/presenters/summary/pdf_presenter.rb
@@ -4,6 +4,7 @@ module Summary
 
     attr_reader :c100_application
     attr_reader :pdf_generator
+    attr_reader :collected_forms
 
     delegate :to_pdf, :pdf_data_rendered?, to: :pdf_generator
 
@@ -17,12 +18,13 @@ module Summary
     # Once all the required forms have been generated, it is neccessary to call
     # the instance method `#to_pdf` to return the final, combined PDF.
     #
-    def generate(*args)
+    def generate(*args, mode: :pdf)
       forms = args.presence || DEFAULT_BUNDLE_FORMS
+      @collected_forms = [] if mode == :html
 
-      generate_c100_form if forms.include?(:c100)
-      generate_c1a_form  if forms.include?(:c1a)
-      generate_c8_form   if forms.include?(:c8)
+      generate_c100_form(mode) if forms.include?(:c100)
+      generate_c1a_form(mode)  if forms.include?(:c1a)
+      generate_c8_form(mode)   if forms.include?(:c8)
     end
 
     def filename
@@ -31,10 +33,8 @@ module Summary
 
     private
 
-    def generate_c100_form
-      pdf_generator.generate(
-        Summary::C100Form.new(c100_application), copies: 1
-      )
+    def generate_c100_form(mode = :pdf)
+      process_form(Summary::C100Form.new(c100_application), mode)
     end
 
     def has_abuse_concerns_data?
@@ -42,37 +42,42 @@ module Summary
         c100_application.abuse_concerns.any? { |abuse| abuse.answer.to_s.eql? 'yes' }
     end
 
-    def generate_c1a_form
+    def generate_c1a_form(mode = :pdf)
       return unless has_abuse_concerns_data?
 
-      add_blank_page_if_needed
-
-      pdf_generator.generate(
-        Summary::C1aForm.new(c100_application), copies: 1
-      )
+      add_blank_page_if_needed(mode)
+      process_form(Summary::C1aForm.new(c100_application), mode)
     end
 
-    def generate_c8_form
+    def generate_c8_form(mode = :pdf)
       if PrivacyChange.changes_apply?
         return unless c100_application.confidentiality_enabled? || c100_application.other_confidentiality_enabled?
       else
         return unless c100_application.confidentiality_enabled?
       end
 
-      add_blank_page_if_needed
-
-      pdf_generator.generate(
-        Summary::C8Form.new(c100_application), copies: 1
-      )
+      add_blank_page_if_needed(mode)
+      process_form(Summary::C8Form.new(c100_application), mode)
     end
 
     # Avoid adding unnecessary blank pages if there are no preceding forms,
     # for example in the case we are generating individual forms like the C8.
     #
-    def add_blank_page_if_needed
-      pdf_generator.generate(
-        Summary::BlankPage.new(c100_application), copies: 1
-      ) if pdf_data_rendered?
+
+    def add_blank_page_if_needed(mode)
+      if mode == :pdf
+        return unless pdf_data_rendered?
+      end
+
+      process_form(Summary::BlankPage.new(c100_application), mode)
+    end
+
+    def process_form(form, mode)
+      if mode == :pdf
+        pdf_generator.generate(form, copies: 1)
+      else
+        @collected_forms << form
+      end
     end
   end
 end
