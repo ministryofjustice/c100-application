686855415ae8836c2837f3b537338a160a1bb055
kalvinlyu <kalvin.lyu@solirius.com>
2025-06-19 14:24:43 +0100

    RST-7370 rename to chelmsford rollout

--- app/controllers/concerns/prl_court_rollout.rb
+++
@@ -1,7 +0,0 @@
-module PrlCourtRollout
-  extend ActiveSupport::Concern
-
-  def self.changes_apply?
-    Time.zone.now.in_time_zone('London') >= Rails.application.config.prl_court_rollout
-  end
-end
