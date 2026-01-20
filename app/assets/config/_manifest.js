00d40a3ceef3fe746ca19962a4f2aae6c2daae7f
Petr Zaparka <petr@zaparka.cz>
2026-01-20 10:33:38 +0000

    Uodating build config and styles.

--- app/assets/config/_manifest.js
+++ app/assets/config/_manifest.js
@@ -1,4 +1,4 @@
 //= link_tree ../builds
 //= link_tree ../images
+//= link_tree ../fonts
 //= link govuk-frontend/dist/govuk/govuk-frontend.min.js
-//= link_tree govuk-frontend/dist/govuk/assets
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: "production",
+  devtool: "source-map",
   entry: {
     application: './app/javascript/application.js'
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'app/assets/builds'),
-    clean: false
+    filename: 'bundle.js',
+    sourceMapFilename: "bundle.map",
+    chunkFormat: "module",
+    path: path.resolve(__dirname, 'app/assets/builds')
   },
   plugins: [
     new webpack.ProvidePlugin({
