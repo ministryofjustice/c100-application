2a790f02c7efdf1fddf6e5c0924bd0e490fc7adf
Petr Zaparka <petr@zaparka.cz>
2026-02-24 15:15:20 +0000

    Fixing lowercase answers

--- app/presenters/summary/sections/application_reasons.rb
+++ app/presenters/summary/sections/application_reasons.rb
@@ -14,7 +14,7 @@ module Summary
           Answer.new(:permission_sought, c100.permission_sought, default: :not_required),
           FreeTextAnswer.new(:permission_details, c100.permission_details),
           FreeTextAnswer.new(:application_details, c100.application_details),
-          FreeTextAnswer.new(:existing_court_order, c100.existing_court_order),
+          Answer.new(:existing_court_order, c100.existing_court_order),
           FreeTextAnswer.new(:court_order_case_number, c100.court_order_case_number),
           FreeTextAnswer.new(:court_order_expiry_date, c100.court_order_expiry_date),
           Answer.new(:existing_court_order_uploadable, c100.existing_court_order_uploadable),
