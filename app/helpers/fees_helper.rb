0cdfbf476f12d8bec5bc44ad4995c8bb7c87c9f6
kalvinlyu <kalvin.lyu@solirius.com>
2026-06-10 10:28:31 +0100

    RST-8032 code improvement

--- app/helpers/fees_helper.rb
+++ app/helpers/fees_helper.rb
@@ -1,14 +1,12 @@
 module FeesHelper
   def fee_amount
-    if FeeIncrease.changes_apply?
-      ActionController::Base.helpers.number_to_currency(
-        Rails.configuration.x.court_fee.new_amount_in_pence / 100
-      )
-    else
-      ActionController::Base.helpers.number_to_currency(
-        Rails.configuration.x.court_fee.amount_in_pence / 100
-      )
-    end
+    ActionController::Base.helpers.number_to_currency(
+      (if FeeIncrease.changes_apply?
+         Rails.configuration.x.court_fee.new_amount_in_pence
+       else
+         Rails.configuration.x.court_fee.amount_in_pence
+       end) / 100
+    )
   end
 
   def phase_banner_copy
