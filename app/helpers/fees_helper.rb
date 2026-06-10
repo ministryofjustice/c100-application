7c81cda788b9ddcecbcfdb3a0432edb371840f58
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2026-06-10 10:49:41 +0100

    refactor to improve code

--- app/helpers/fees_helper.rb
+++ app/helpers/fees_helper.rb
@@ -1,12 +1,11 @@
 module FeesHelper
   def fee_amount
-    ActionController::Base.helpers.number_to_currency(
-      (if FeeIncrease.changes_apply?
-         Rails.configuration.x.court_fee.new_amount_in_pence
-       else
-         Rails.configuration.x.court_fee.amount_in_pence
-       end) / 100
-    )
+    amount = if FeeIncrease.changes_apply?
+               Rails.configuration.x.court_fee.new_amount_in_pence
+             else
+               Rails.configuration.x.court_fee.amount_in_pence
+             end
+    ActionController::Base.helpers.number_to_currency(amount / 100)
   end
 
   def phase_banner_copy
