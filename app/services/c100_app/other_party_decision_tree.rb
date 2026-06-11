d4950d2bad834c35bde6c755fd18e30993f30d94
kalvinlyu <kalvin.lyu@solirius.com>
2026-06-11 15:07:10 +0100

    RST-8241 rubocop

--- app/services/c100_app/other_party_decision_tree.rb
+++ app/services/c100_app/other_party_decision_tree.rb
@@ -1,6 +1,6 @@
 module C100App
   class OtherPartyDecisionTree < PeopleDecisionTree
-    # rubocop:disable Metrics/MethodLength, Metrics/AbcSize, Metrics/CyclomaticComplexity
+    # rubocop:disable Metrics/MethodLength
     def destination
       return next_step if next_step
 
@@ -27,7 +27,7 @@ module C100App
         raise InvalidStep, "Invalid step '#{as || step_params}'"
       end
     end
-    # rubocop:enable Metrics/MethodLength, Metrics/AbcSize, Metrics/CyclomaticComplexity:
+    # rubocop:enable Metrics/MethodLength
 
     private
 
